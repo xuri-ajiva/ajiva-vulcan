08c7557ef93587b34c65744ff92d0f72d77409f3
xuri02 <t0bl4s.durst@gmail.com>
2021-01-06 20:53:39 +0100

    added mem util

---
+++ ajiva/Models/MemoryUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace vulcan_01
+{
+    public static class MemoryUtil
+    {
+        
+        [DllImport("kernel32.dll", EntryPoint = "CopyMemory", SetLastError = false)]
+        public static extern void CopyMemory(IntPtr dest, IntPtr src, uint count);
+        
+        [DllImport("kernel32.dll", EntryPoint = "CopyMemory", SetLastError = false)]
+        public static extern void Copy(IntPtr dest, IntPtr src, uint count);
+    }
+}
