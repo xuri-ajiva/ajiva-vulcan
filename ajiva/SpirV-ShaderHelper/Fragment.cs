2b89862ef17e50ac93dfc441eeafe8ab60d98a7b
xuri02 <t0bl4s.durst@gmail.com>
2021-01-06 20:57:21 +0100

    added spir-v linq helper classes

---
+++ ajiva/SpirV-ShaderHelper/Fragment.cs
@@ -0,0 +1,15 @@
+﻿using GlmSharp;
+using SharpVk.Shanq;
+
+namespace ajiva
+{
+    internal struct FragmentInput
+    {
+        [Location(0)] public vec3 Colour;
+        [Location(1)] public vec4 Position;
+    }
+    internal struct FragmentOutput
+    {
+        [Location(0)] public vec4 Colour;
+    }
+}
---
+++ ajiva/SpirV-ShaderHelper/VertexOutput.cs
@@ -0,0 +1,13 @@
+﻿using GlmSharp;
+using SharpVk.Shanq;
+using SharpVk.Spirv;
+
+namespace ajiva
+{
+    internal struct VertexOutput
+    {
+        [Location(0)] public vec3 Colour;
+
+        [BuiltIn(BuiltIn.Position), Location(1)] public vec4 Position;
+    }
+}
