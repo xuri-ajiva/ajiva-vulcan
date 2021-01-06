518c4dd387d1677e630a4098028043a640eecb0a
xuri02 <t0bl4s.durst@gmail.com>
2021-01-06 20:52:09 +0100

    added base entity for ajiva engine: AEntity

---
+++ ajiva/Entitys/AEntity.cs
@@ -0,0 +1,15 @@
+﻿
+using vulcan_01.Entitys;
+
+namespace ajiva.Entitys
+{
+    public class AEntity
+    {
+        public AEntity(Transform3d transform)
+        {
+            Transform = transform;
+        }
+
+        public Transform3d Transform;
+    }
+}
---
+++ ajiva/Entitys/Transform.cs
@@ -0,0 +1,23 @@
+﻿using GlmSharp;
+
+namespace vulcan_01.Entitys
+{
+    public struct Transform3d
+    {
+        public Transform3d(vec3 position, vec3 rotation, vec3 scale)
+        {
+            this.Position = position;
+            this.Rotation = rotation;
+            this.Scale = scale;
+        }
+
+        public Transform3d(vec3 position, vec3 rotation) : this(position, rotation, Default.Scale) { }
+        public Transform3d(vec3 position) : this(position, Default.Rotation) { }
+
+        public vec3 Position;
+        public vec3 Rotation;
+        public vec3 Scale;
+        
+        public static readonly Transform3d Default = new(vec3.Zero, vec3.Zero, vec3.Ones);
+    }
+}
