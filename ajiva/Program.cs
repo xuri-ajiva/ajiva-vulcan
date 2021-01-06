37c0cb3bc7fec47e922b8a073a82e98f34b71169
xuri02 <t0bl4s.durst@gmail.com>
2021-01-06 18:23:04 +0100

    second commit

---
+++ ajiva/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ajiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
