7b617787c018145ffcc9d36c477656f490557a67
xuri02 <t0bl4s.durst@gmail.com>
2021-01-07 22:52:58 +0100

    added uniform buffer model

---
+++ ajiva/Models/UniformBuffer.cs
@@ -0,0 +1,11 @@
+﻿using GlmSharp;
+
+namespace ajiva.Models
+{
+    internal struct UniformBufferObject
+    {
+        public mat4 Model;
+        public mat4 View;
+        public mat4 Proj;
+    };
+}
