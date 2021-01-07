0de4ad3cfdc1e6b5bfe41a24dfa19dd301e2eb95
xuri02 <t0bl4s.durst@gmail.com>
2021-01-07 22:53:21 +0100

    added IEngin interface

---
+++ ajiva/Engine/IEngine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq.Expressions;
+using ajiva.EngineManagers;
+using ajiva.Models;
+using SharpVk;
+using SharpVk.Khronos;
+
+namespace ajiva.Engine
+{
+    public interface IEngine
+    {
+        Instance? Instance { get; }
+        DeviceManager DeviceManager { get; }
+
+        SwapChainManager SwapChainManager { get; }
+
+        IPlatformWindow Window { get; }
+
+        ImageManager ImageManager { get; }
+
+        GraphicsManager GraphicsManager { get; }
+        ShaderManager ShaderManager { get; }
+        BufferManager BufferManager { get; }
+        SemaphoreManager SemaphoreManager { get; }
+        TextureManager TextureManager { get; }
+
+        public T NotNull<T>(Expression<Func<T?>> action)
+        {
+            var expression = (MemberExpression)action.Body;
+            string name = expression.Member.Name;
+            var res = action.Compile()();
+            if (res == null)
+            {
+                throw new TypeInitializationException(typeof(T).FullName, new ArgumentException(name));
+            }
+            return res;
+        }
+
+        public void Dependent<T>(T obj, string name)
+        {
+            if (obj == null)
+            {
+                throw new TypeInitializationException(typeof(T).FullName, new ArgumentException(name));
+            }
+        }
+    }
+}
