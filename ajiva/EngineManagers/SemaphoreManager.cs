51435979496e7e1943a61f1e133e488a0f6dedf2
xuri02 <t0bl4s.durst@gmail.com>
2021-01-07 22:49:23 +0100

    added semaphore manager

---
+++ ajiva/EngineManagers/SemaphoreManager.cs
@@ -0,0 +1,23 @@
+﻿using ajiva.Engine;
+using SharpVk;
+
+namespace ajiva.EngineManagers
+{
+    public class SemaphoreManager : IEngineManager
+    {
+        private readonly IEngine engine;
+
+        public Semaphore ImageAvailable { get; private set; }
+        public Semaphore RenderFinished { get; private set; }
+
+        public SemaphoreManager(IEngine engine)
+        {
+            this.engine = engine;
+        }
+        public void CreateSemaphores()
+        {
+            ImageAvailable = engine.DeviceManager.Device.CreateSemaphore();
+            RenderFinished = engine.DeviceManager.Device.CreateSemaphore();
+        }
+    }
+}
