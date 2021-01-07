2a705a7e3a571f839e5d740a4caaffe1ca1e36b4
xuri02 <t0bl4s.durst@gmail.com>
2021-01-07 23:25:34 +0100

    added static vectors and indices

---
+++ ajiva/FutureChange.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using ajiva.Models;
+using GlmSharp;
+
+namespace ajiva
+{
+    public partial class Program
+    {
+        private readonly ushort[] indices =
+        {
+            0, 1, 2, 2, 3, 0,
+        };
+        private readonly Vertex[] vertices = new[]
+        {
+            new Vertex(new vec2(-0.5f, -0.5f), new(1.0f, 0.0f, 0.0f), new(1.0f, 0.0f)), new Vertex(new vec2(0.5f, -0.5f), new(0.0f, 1.0f, 0.0f), new(0.0f, 0.0f)), new Vertex(new vec2(0.5f, 0.5f), new(0.0f, 0.0f, 1.0f), new(0.0f, 1.0f)), new Vertex(new vec2(-0.5f, 0.5f), new(1.0f, 1.0f, 1.0f), new(1.0f, 1.0f))
+        };
+    }
+}
