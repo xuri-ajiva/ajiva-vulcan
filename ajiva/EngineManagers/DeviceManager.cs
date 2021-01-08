8da0637ffbf2d5a5148afd1f9f7990f3e66f97fd
xuri02 <t0bl4s.durst@gmail.com>
2021-01-08 18:58:55 +0100

    moved QueueFamilyIndices to its own file

--- ajiva/EngineManagers/DeviceManager.cs
+++ ajiva/EngineManagers/DeviceManager.cs
@@ -104,40 +104,7 @@ namespace ajiva.EngineManagers
 
         public void WaitIdle()
         {
-            Device?.WaitIdle();
-        }
-
-        public struct QueueFamilyIndices
-        {
-            public uint? GraphicsFamily;
-            public uint? PresentFamily;
-            public uint? TransferFamily;
-
-            public IEnumerable<uint> Indices
-            {
-                get
-                {
-                    if (GraphicsFamily.HasValue)
-                    {
-                        yield return GraphicsFamily.Value;
-                    }
-
-                    if (PresentFamily.HasValue && PresentFamily != GraphicsFamily)
-                    {
-                        yield return PresentFamily.Value;
-                    }
-
-                    if (TransferFamily.HasValue && TransferFamily != PresentFamily && TransferFamily != GraphicsFamily)
-                    {
-                        yield return TransferFamily.Value;
-                    }
-                }
-            }
-
-            public bool IsComplete =>
-                GraphicsFamily.HasValue
-                && PresentFamily.HasValue
-                && TransferFamily.HasValue;
+            Device.WaitIdle();
         }
 
         public void Dispose()
