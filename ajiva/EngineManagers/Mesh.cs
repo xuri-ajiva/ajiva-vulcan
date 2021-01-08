63668d0a10554f615dd041c727ceffeb620c24ce
xuri02 <t0bl4s.durst@gmail.com>
2021-01-08 18:59:18 +0100

    added mesh

---
+++ ajiva/EngineManagers/Mesh.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ajiva.Models;
+using SharpVk;
+
+namespace ajiva.EngineManagers
+{
+    public class Mesh : IDisposable
+    {
+        private readonly DeviceManager manager;
+        public BufferOfT<Vertex> Vertices { get; }
+        public BufferOfT<ushort> Indeces { get; }
+
+        public Mesh(DeviceManager manager, Vertex[] vertices, ushort[] indices)
+        {
+            this.manager = manager;
+            Vertices = CreateShaderBuffer(vertices, BufferUsageFlags.VertexBuffer);
+            Indeces = CreateShaderBuffer(indices, BufferUsageFlags.IndexBuffer);
+        }
+
+        private BufferOfT<T> CreateShaderBuffer<T>(T[] val, BufferUsageFlags bufferUsage) where T : notnull
+        {
+            BufferOfT<T> aBuffer = new(val);
+            var copyBuffer = CopyBuffer<T>.CreateCopyBufferOnDevice(val, manager);
+
+            aBuffer.Create(manager.Device, BufferUsageFlags.TransferDestination | bufferUsage, type => manager.FindMemoryType(type, MemoryPropertyFlags.DeviceLocal));
+
+            copyBuffer.CopyTo(aBuffer, manager);
+            copyBuffer.Dispose();
+            return aBuffer;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Vertices.Dispose();
+            Indeces.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public void Bind(CommandBuffer commandBuffer)
+        {
+            commandBuffer.BindVertexBuffers(0, Vertices.Buffer, 0);
+            commandBuffer.BindIndexBuffer(Indeces.Buffer, 0, IndexType.Uint16);
+        }
+
+        public void DrawIndexed(CommandBuffer commandBuffer)
+        {
+            commandBuffer.DrawIndexed((uint)Indeces.Length, 1, 0, 0, 0);
+        }
+    }
+}
