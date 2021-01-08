eefa143381d1ce8e7f5635ed7dd31166eb1bf6a5
xuri02 <t0bl4s.durst@gmail.com>
2021-01-08 18:58:28 +0100

    added throw helper to throw if som null abel is null

---
+++ ajiva/Models/Throw.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ajiva.Models
+{
+    public static class Throw
+    {
+        public static void Assert([DoesNotReturnIf(false)] bool condition, string? message)
+        {
+            if (condition) return;
+
+            var callingFrame = new StackTrace().GetFrame(1);
+            throw new TypeInitializationException(callingFrame?.GetMethod()?.DeclaringType?.Name ?? "Unknown", new ArgumentException(message));
+        }
+    }
+}
