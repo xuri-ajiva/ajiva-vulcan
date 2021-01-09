fa26ef1d608a63ae524beefa86bb9039a9501946
xuri02 <t0bl4s.durst@gmail.com>
2021-01-09 19:13:15 +0100

    added more validation layers

--- ajiva/ProgramVulcanInit.cs
+++ ajiva/ProgramVulcanInit.cs
@@ -12,21 +12,30 @@ namespace ajiva
         {
             if (Instance != null) return;
 
-            var enabledLayers = new List<string>();
+            List<string> enabledLayers = new();
 
             //VK_LAYER_LUNARG_api_dump
             //VK_LAYER_LUNARG_standard_validation
 
+            var props = Instance.EnumerateLayerProperties();
             void AddAvailableLayer(string layerName)
             {
-                if (SharpVk.Instance.EnumerateLayerProperties().Any(x => x.LayerName == layerName))
+                if (props.Any(x => x.LayerName == layerName))
                     enabledLayers.Add(layerName);
             }
 
             AddAvailableLayer("VK_LAYER_LUNARG_standard_validation");
             AddAvailableLayer("VK_LAYER_KHRONOS_validation");
+            AddAvailableLayer("VK_LAYER_GOOGLE_unique_objects");
+            //AddAvailableLayer("VK_LAYER_LUNARG_api_dump");
+            AddAvailableLayer("VK_LAYER_LUNARG_core_validation");
+            AddAvailableLayer("VK_LAYER_LUNARG_image");
+            AddAvailableLayer("VK_LAYER_LUNARG_object_tracker");
+            AddAvailableLayer("VK_LAYER_LUNARG_parameter_validation");
+            AddAvailableLayer("VK_LAYER_LUNARG_swapchain");
+            AddAvailableLayer("VK_LAYER_GOOGLE_threading");
 
-            Instance = SharpVk.Instance.Create(
+            Instance = Instance.Create(
                 enabledLayers.ToArray(),
                 Window.GetRequiredInstanceExtensions().Append(ExtExtensions.DebugReport).ToArray(),
                 applicationInfo: new ApplicationInfo
@@ -38,7 +47,7 @@ namespace ajiva
                     ApiVersion = new(1, 0, 0)
                 });
 
-            Instance.CreateDebugReportCallback(DebugReportDelegate, DebugReportFlags.Error | DebugReportFlags.Warning | DebugReportFlags.Information | DebugReportFlags.PerformanceWarning);
+            Instance.CreateDebugReportCallback(DebugReportDelegate, DebugReportFlags.Error | DebugReportFlags.Warning | DebugReportFlags.PerformanceWarning);
 
             //foreach (var extension in SharpVk.Instance.EnumerateExtensionProperties())
             //    Console.WriteLine($"Extension available: {extension.ExtensionName}");
