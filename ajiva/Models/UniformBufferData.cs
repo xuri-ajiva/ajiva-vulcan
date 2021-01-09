e6501715cac2a5c35b512d562cd0f845f6c49c06
xuri02 <t0bl4s.durst@gmail.com>
2021-01-09 19:03:16 +0100

    split uniform buffer data

---
+++ ajiva/Models/UniformBufferData.cs
@@ -0,0 +1,20 @@
+﻿using GlmSharp;
+
+namespace ajiva.Models
+{
+    public struct UniformBufferData
+    {
+        public mat4 Model;
+        public mat4 View;
+        public mat4 Proj;
+    };
+    public struct UniformModel
+    {
+        public mat4 Model;
+    };
+    public struct UniformViewProj
+    {
+        public mat4 View;
+        public mat4 Proj;
+    };
+}
