31a88d0134d76b9280a0b37feb6bbefa6c711dd8
xuri02 <t0bl4s.durst@gmail.com>
2021-01-09 19:03:32 +0100

    added writable buffer

---
+++ ajiva/Models/WritableCopyBuffer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ajiva.Models
+{
+    public class WritableCopyBuffer<T> : CopyBuffer<T> where T : notnull
+    {
+        /// <inheritdoc />
+        public WritableCopyBuffer(T[] val) : base(val)
+        {
+        }
+
+        public void Update(T[] newData)
+        {
+            if (newData.Length > Value.Length)
+            {
+                throw new ArgumentException("Curently you can only update the data, not add some", nameof(newData));
+            }
+
+            Value = newData;
+            CopyValueToBuffer();
+        }
+    }
+}
