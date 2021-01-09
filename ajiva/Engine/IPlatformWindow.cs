56b9c21e4a835770fe7535a8c1a9599086e791b7
xuri02 <t0bl4s.durst@gmail.com>
2021-01-09 20:30:11 +0100

    moved IPlatformWindow into separate file

---
+++ ajiva/Engine/IPlatformWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using GlmSharp;
+using SharpVk.Khronos;
+
+namespace ajiva.EngineManagers
+{
+    public interface IPlatformWindow : IDisposable
+    {
+        public void InitWindow(int surfaceWidth, int surfaceHeight);
+        public IEnumerable<string> GetRequiredInstanceExtensions();
+        public void CreateSurface();
+        public void MainLoop(TimeSpan timeToRun);
+        public void CloseWindow();
+        public event PlatformEventHandler OnFrame;
+        public event KeyEventHandler OnKeyEvent;
+        public event EventHandler OnResize;
+        public event EventHandler<vec2> OnMouseMove;
+
+        public Surface Surface { get; }
+        uint SurfaceWidth { get; }
+        uint SurfaceHeight { get; }
+    }
+}
--- ajiva/EngineManagers/PlatformWindow.cs
+++ ajiva/EngineManagers/PlatformWindow.cs
@@ -172,22 +172,6 @@ namespace ajiva.EngineManagers
             Dispose(false);
         }
     }
-    public interface IPlatformWindow : IDisposable
-    {
-        public void InitWindow(int surfaceWidth, int surfaceHeight);
-        public IEnumerable<string> GetRequiredInstanceExtensions();
-        public void CreateSurface();
-        public void MainLoop(TimeSpan timeToRun);
-        public void CloseWindow();
-        public event PlatformEventHandler OnFrame;
-        public event KeyEventHandler OnKeyEvent;
-        public event EventHandler OnResize;
-        public event EventHandler<vec2> OnMouseMove;
-
-        public Surface Surface { get; }
-        uint SurfaceWidth { get; }
-        uint SurfaceHeight { get; }
-    }
 
     public delegate void KeyEventHandler(object? sender, Key key, int scancode, InputAction inputAction, Modifier modifiers);
 
