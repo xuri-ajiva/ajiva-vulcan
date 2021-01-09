a0f6381abe5536589af2f8221d08dbb5cd56c5ab
xuri02 <t0bl4s.durst@gmail.com>
2021-01-09 19:07:21 +0100

    finalized engine null check

--- ajiva/Engine/IEngine.cs
+++ ajiva/Engine/IEngine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using ajiva.EngineManagers;
 using SharpVk;
 
@@ -12,30 +13,33 @@ namespace ajiva.Engine
 
         Instance? Instance { get; }
         DeviceManager DeviceManager { get; }
-
         SwapChainManager SwapChainManager { get; }
-
         IPlatformWindow Window { get; }
-
         ImageManager ImageManager { get; }
-
         GraphicsManager GraphicsManager { get; }
         ShaderManager ShaderManager { get; }
         BufferManager BufferManager { get; }
         SemaphoreManager SemaphoreManager { get; }
         TextureManager TextureManager { get; }
 
-        public T NotNull<T>(Expression<Func<T?>> action)
+#pragma warning disable 8763
+        [DoesNotReturn, MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        public IEngine Reacquiring<T>(Expression<Func<T?>> path, bool required)
         {
-            var expression = (MemberExpression)action.Body;
+            var expression = (MemberExpression)path.Body;
             string name = expression.Member.Name;
-            var res = action.Compile()();
-            if (res == null)
+            var res = path.Compile()();
+
+            switch (required)
             {
-                throw new TypeInitializationException(typeof(T).FullName, new ArgumentException(name));
+                case true when res == null:
+                    throw new NullReferenceException(typeof(T).FullName, new ArgumentException(name));
+                case false when res != null:
+                    throw new TypeInitializationException(typeof(T).FullName, new ArgumentException(name));
             }
-            return res;
+            return this;
         }
+#pragma warning restore 8763
 
         public void Dependent<T>(T obj, string name)
         {
