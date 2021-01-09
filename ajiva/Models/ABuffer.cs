2a7aa7dfa105aa10c6571a72e1f2d62e261d7e01
xuri02 <t0bl4s.durst@gmail.com>
2021-01-09 19:02:01 +0100

    added more aggressive freeing of memory to buffer

--- ajiva/Models/ABuffer.cs
+++ ajiva/Models/ABuffer.cs
@@ -5,19 +5,11 @@ using Buffer = SharpVk.Buffer;
 
 namespace ajiva.Models
 {
-    public class ABuffer : IDisposable
+    public abstract class ABuffer : IDisposable
     {
         public Buffer? Buffer;
         public DeviceMemory? Memory;
         public readonly uint Size;
-
-        /// <inheritdoc />
-        public void Dispose()
-        {
-            Buffer?.Dispose();
-            Memory?.Free();
-            GC.SuppressFinalize(this);
-        }
 
         public ABuffer(uint size)
         {
@@ -35,10 +27,19 @@ namespace ajiva.Models
             Memory = device.AllocateMemory(memRequirements.Size, memoryTypeIndex(memRequirements.MemoryTypeBits));
             Buffer.BindMemory(Memory, 0);
         }
+
+        /// <inheritdoc />
+        public virtual void Dispose()
+        {
+            Buffer?.Dispose();
+            Memory?.Free();
+            GC.SuppressFinalize(this);
+        }
     }
     public class BufferOfT<T> : ABuffer where T : notnull
     {
         public int Length { get; }
+        public uint SizeOfT { get; }
         public T[] Value { get; protected set; }
 
         /// <inheritdoc />
@@ -46,11 +47,19 @@ namespace ajiva.Models
         {
             Value = val;
             Length = val.Length;
+            SizeOfT = (uint)Unsafe.SizeOf<T>();
         }
 
         public void ClearT()
         {
             Value = Array.Empty<T>();
+        }
+
+        /// <inheritdoc />
+        public override void Dispose()
+        {
+            base.Dispose();
+            ClearT();
         }
     }
 }
