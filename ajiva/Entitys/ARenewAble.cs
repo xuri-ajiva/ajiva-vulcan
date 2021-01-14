48b832f146b44505be05248fbda2ed6df4d69e07
xuri02 <t0bl4s.durst@gmail.com>
2021-01-14 21:11:00 +0100

    added renewable for render engine to render it, part of AEntity

---
+++ ajiva/Entitys/ARenewAble.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using ajiva.Engine;
+using ajiva.Models;
+using SharpVk;
+
+namespace ajiva.Entitys
+{
+    public class ARenewAble : DisposingLogger
+    {
+        private IRenderEngine? render;
+        public Mesh? Mesh { get; private set; }
+
+        public ARenewAble(Mesh? mesh)
+        {
+            Mesh = mesh;
+        }
+
+        public void BindAndDraw(CommandBuffer commandBuffer)
+        {
+            ATrace.Assert(Mesh != null, nameof(Mesh) + " != null");
+            ATrace.Assert(render != null, nameof(render) + " != null");
+
+            Mesh.Bind(commandBuffer);
+            
+            commandBuffer.BindDescriptorSets(PipelineBindPoint.Graphics, render.GraphicsComponent.PipelineLayout, 0, render.GraphicsComponent.DescriptorSet, null);
+
+            Mesh.DrawIndexed(commandBuffer);
+        }
+
+        public void Create(IRenderEngine renderEngine)
+        {
+            render = renderEngine;
+            Mesh?.Create(renderEngine.DeviceComponent);
+        }
+
+        /// <inheritdoc />
+        protected override void ReleaseUnmanagedResources()
+        {
+            Mesh?.Dispose();
+        }
+    }
+}
