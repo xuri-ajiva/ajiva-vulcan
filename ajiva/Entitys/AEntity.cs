b59396747c1b8da0920caaf9c30b714fdfcb775a
xuri02 <t0bl4s.durst@gmail.com>
2021-01-14 21:11:35 +0100

    changed entity to take renewable in ctor

--- ajiva/Entitys/AEntity.cs
+++ ajiva/Entitys/AEntity.cs
@@ -1,13 +1,24 @@
-﻿
+﻿using ajiva.Engine;
+
 namespace ajiva.Entitys
 {
-    public class AEntity
+    public class AEntity : DisposingLogger
     {
-        public AEntity(Transform3d transform)
+        public AEntity(Transform3d transform, ARenewAble renewAble)
         {
             Transform = transform;
+            RenewAble = renewAble;
         }
 
         public Transform3d Transform;
+
+        public readonly ARenewAble RenewAble;
+
+        /// <inheritdoc />
+        protected override void ReleaseUnmanagedResources()
+        {
+            Transform = Transform3d.Default;
+            RenewAble.Dispose();
+        }
     }
 }
