aa8d40e962d4219f610b4d749b801bbe725b2be1
xuri02 <t0bl4s.durst@gmail.com>
2021-01-14 21:02:22 +0100

    added logger and disposing handler

---
+++ ajiva/Engine/DisposingLogger.cs
@@ -0,0 +1,46 @@
+﻿//#define LOGGING_TRUE
+using System;
+
+namespace ajiva.Engine
+{
+    public abstract class DisposingLogger : IDisposable
+    {
+#if LOGGING_TRUE
+
+        public DisposingLogger()
+        {
+            Console.WriteLine($"Created: {GetType()}");
+        }
+#endif
+#region IDisposable
+
+        protected abstract void ReleaseUnmanagedResources();
+
+        protected virtual void Dispose(bool disposing)
+        {
+            ReleaseUnmanagedResources();
+            if (disposing)
+            {
+            }
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+#if LOGGING_TRUE
+            Console.WriteLine($"Disposed: {GetType()}");
+#endif
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <inheritdoc />
+        ~DisposingLogger()
+        {
+            Console.WriteLine($"Deleted: {GetType()}");
+            Dispose(false);
+        }
+
+#endregion
+    }
+}
