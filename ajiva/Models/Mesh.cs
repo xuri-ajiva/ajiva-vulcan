813c94334159a54c5dccdba5914bc41bbb14fa2c
xuri02 <t0bl4s.durst@gmail.com>
2021-01-14 21:16:40 +0100

    changed mesh logic to have an create and not create all buffers in the ctor

--- ajiva/Models/Mesh.cs
+++ ajiva/Models/Mesh.cs
@@ -1,50 +1,64 @@
 ﻿using System;
+using System.Diagnostics;
+using ajiva.Engine;
 using ajiva.EngineManagers;
 using SharpVk;
 
 namespace ajiva.Models
 {
-    public class Mesh : IDisposable
+    public partial class Mesh : DisposingLogger
     {
-        private readonly DeviceManager manager;
-        public BufferOfT<Vertex> Vertices { get; }
-        public BufferOfT<ushort> Indeces { get; }
+        public readonly Vertex[] VerticesData;
+        public readonly ushort[] IndicesData;
+        private DeviceComponent? deviceComponent;
+        public BufferOfT<Vertex>? Vertices { get; protected set; }
+        public BufferOfT<ushort>? Indeces { get; protected set; }
 
-        public Mesh(DeviceManager manager, Vertex[] vertices, ushort[] indices)
+        public Mesh(Vertex[] verticesData, ushort[] indicesData)
         {
-            this.manager = manager;
-            Vertices = CreateShaderBuffer(vertices, BufferUsageFlags.VertexBuffer);
-            Indeces = CreateShaderBuffer(indices, BufferUsageFlags.IndexBuffer);
+            this.VerticesData = verticesData;
+            this.IndicesData = indicesData;
+        }
+
+        public void Create(DeviceComponent component)
+        {
+            this.deviceComponent = component;
+            Vertices = CreateShaderBuffer(VerticesData, BufferUsageFlags.VertexBuffer);
+            Indeces = CreateShaderBuffer(IndicesData, BufferUsageFlags.IndexBuffer);
         }
 
         private BufferOfT<T> CreateShaderBuffer<T>(T[] val, BufferUsageFlags bufferUsage) where T : notnull
         {
+            ATrace.Assert(deviceComponent != null, nameof(deviceComponent) + " != null");
+            
             BufferOfT<T> aBuffer = new(val);
-            var copyBuffer = CopyBuffer<T>.CreateCopyBufferOnDevice(val, manager);
+            var copyBuffer = CopyBuffer<T>.CreateCopyBufferOnDevice(val, deviceComponent);
 
-            aBuffer.Create(manager, BufferUsageFlags.TransferDestination | bufferUsage,  MemoryPropertyFlags.DeviceLocal);
+            aBuffer.Create(deviceComponent, BufferUsageFlags.TransferDestination | bufferUsage, MemoryPropertyFlags.DeviceLocal);
 
-            copyBuffer.CopyTo(aBuffer, manager);
+            copyBuffer.CopyTo(aBuffer, deviceComponent);
             copyBuffer.Dispose();
             return aBuffer;
         }
 
         /// <inheritdoc />
-        public void Dispose()
+        protected override void ReleaseUnmanagedResources()
         {
-            Vertices.Dispose();
-            Indeces.Dispose();
-            GC.SuppressFinalize(this);
+            Vertices?.Dispose();
+            Indeces?.Dispose();
         }
 
         public void Bind(CommandBuffer commandBuffer)
         {
+            ATrace.Assert(Vertices != null, nameof(Vertices) + " != null");
+            ATrace.Assert(Indeces != null, nameof(Indeces) + " != null");
             commandBuffer.BindVertexBuffers(0, Vertices.Buffer, 0);
             commandBuffer.BindIndexBuffer(Indeces.Buffer, 0, IndexType.Uint16);
         }
 
         public void DrawIndexed(CommandBuffer commandBuffer)
         {
+            ATrace.Assert(Indeces != null, nameof(Indeces) + " != null");
             commandBuffer.DrawIndexed((uint)Indeces.Length, 1, 0, 0, 0);
         }
     }
