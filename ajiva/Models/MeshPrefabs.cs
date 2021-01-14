2c1b975590a2bdc8cca1b6dec56d9eee6b31bfc9
xuri02 <t0bl4s.durst@gmail.com>
2021-01-14 21:16:54 +0100

    created mesh prefabs like cube

---
+++ ajiva/Models/MeshPrefabs.cs
@@ -0,0 +1,29 @@
+﻿using ajiva.Entitys;
+using GlmSharp;
+
+namespace ajiva.Models
+{
+    public partial class Mesh
+    {
+        public static implicit operator ARenewAble(Mesh? mesh) => new(mesh);
+        
+        public static Mesh Cube { get; } = new(new Vertex[]
+        {
+            new(new(-1.0f, -1.0f, 1.0f), new(1.0f, 0.0f, 0.0f)),
+            new(new(1.0f, -1.0f, 1.0f), new(0.0f, 1.0f, 0.0f)),
+            new(new(1.0f, 1.0f, 1.0f), new(0.0f, 0.0f, 1.0f)),
+            new(new(-1.0f, 1.0f, 1.0f), new(0.0f, 0.0f, 0.0f)),
+            new(new(-1.0f, -1.0f, -1.0f), new(1.0f, 0.0f, 0.0f)),
+            new(new(1.0f, -1.0f, -1.0f), new(0.0f, 1.0f, 0.0f)),
+            new(new(1.0f, 1.0f, -1.0f), new(0.0f, 0.0f, 1.0f)),
+            new(new(-1.0f, 1.0f, -1.0f), new(0.0f, 0.0f, 0.0f)),
+        }, new ushort[]
+        {
+            0, 1, 2, 2, 3, 0, 1, 5, 6,
+            6, 2, 1, 7, 6, 5, 5, 4, 7,
+            4, 0, 3, 3, 7, 4, 4, 5, 1,
+            1, 0, 4, 3, 2, 6, 6, 7, 3,
+        });
+        public static Mesh? Empty { get; } = null;
+    }
+}
