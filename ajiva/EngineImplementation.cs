fd6e4fff86cd0fcb501c4cc3322cd7642b1721d7
xuri02 <t0bl4s.durst@gmail.com>
2021-01-14 21:06:00 +0100

    moved init / dispose and recreating stuff

--- ajiva/EngineImplementation.cs
+++ ajiva/EngineImplementation.cs
@@ -43,6 +43,180 @@ namespace ajiva
         /// <inheritdoc />
         public SemaphoreManager SemaphoreManager { get; }
         /// <inheritdoc />
-        public TextureManager TextureManager { get; }
+        public TextureComponent TextureComponent { get; }
+        
+       
+        /// <inheritdoc />
+        public object Lock { get; } = new();
+
+        #region Public
+
+        #region Vars
+
+        private static readonly DebugReportCallbackDelegate DebugReportDelegate = DebugReport;
+
+        public long InitialTimestamp;
+
+        #endregion
+
+        private static Bool32 DebugReport(DebugReportFlags flags, DebugReportObjectType objectType, ulong @object, HostSize location, int messageCode, string layerPrefix, string message, IntPtr userData)
+        {
+            Console.WriteLine($"[{flags}] ({objectType}) {layerPrefix}:\n{message}");
+
+            return false;
+        }
+
+        public void Cleanup()
+        {
+            Dispose();
+        }
+
+        private void CleanupSwapChain()
+        {
+            ImageComponent.DepthImage.Dispose();
+
+            SwapChainComponent.CleanupSwapChain();
+
+            DeviceComponent.FreeCommandBuffers();
+
+            GraphicsComponent.Pipeline.Dispose();
+
+            GraphicsComponent.PipelineLayout.Dispose();
+
+            GraphicsComponent.RenderPass.Dispose();
+
+            GraphicsComponent.DescriptorPool.Dispose();
+        }
+
+        public void RecreateSwapChain()
+        {
+            lock (Lock)
+            {
+                DeviceComponent.WaitIdle();
+                CleanupSwapChain();
+
+                SwapChainComponent.CreateSwapChain();
+                SwapChainComponent.CreateImageViews();
+                ImageComponent.CreateDepthResources();
+                GraphicsComponent.CreateRenderPass();
+                SwapChainComponent.CreateFrameBuffers();
+                //bufferManager.CreateUniformBuffer();   free in cleanup swapchain if created here
+                GraphicsComponent.CreateGraphicsPipeline();
+                GraphicsComponent.CreateDescriptorPool();
+                GraphicsComponent.CreateDescriptorSet();
+                DeviceComponent.CreateCommandBuffers();
+            }
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            if (!Runing) return;
+            Runing = false;
+
+            DeviceComponent.WaitIdle();
+
+            SwapChainComponent.Dispose();
+            ImageComponent.Dispose();
+            GraphicsComponent.Dispose();
+            ShaderComponent.Dispose();
+            AEnittyComponent.Dispose();
+            SemaphoreComponent.Dispose();
+            TextureComponent.Dispose();
+            Window.Dispose();
+            DeviceComponent.Dispose();
+            Runing = false;
+            GC.Collect();
+        }
+
+  #endregion
+
+        public static (Instance instance, DebugReportCallback debugReportCallback) CreateInstance(IEnumerable<string> enabledExtensionNames)
+        {
+            //if (Instance != null) return;
+
+            List<string> enabledLayers = new();
+
+            var props = Instance.EnumerateLayerProperties();
+
+            void AddAvailableLayer(string layerName)
+            {
+                if (props.Any(x => x.LayerName == layerName))
+                    enabledLayers.Add(layerName);
+            }
+
+            AddAvailableLayer("VK_LAYER_LUNARG_standard_validation");
+            AddAvailableLayer("VK_LAYER_KHRONOS_validation");
+            AddAvailableLayer("VK_LAYER_GOOGLE_unique_objects");
+            //AddAvailableLayer("VK_LAYER_LUNARG_api_dump");
+            AddAvailableLayer("VK_LAYER_LUNARG_core_validation");
+            AddAvailableLayer("VK_LAYER_LUNARG_image");
+            AddAvailableLayer("VK_LAYER_LUNARG_object_tracker");
+            AddAvailableLayer("VK_LAYER_LUNARG_parameter_validation");
+            AddAvailableLayer("VK_LAYER_LUNARG_swapchain");
+            AddAvailableLayer("VK_LAYER_GOOGLE_threading");
+
+            var instance = Instance.Create(
+                enabledLayers.ToArray(),
+                enabledExtensionNames.Append(ExtExtensions.DebugReport).ToArray(),
+                applicationInfo: new ApplicationInfo
+                {
+                    ApplicationName = "ajiva",
+                    ApplicationVersion = new(0, 0, 1),
+                    EngineName = "ajiva-engine",
+                    EngineVersion = new(0, 0, 1),
+                    ApiVersion = new(1, 0, 0)
+                });
+
+            var debugReportCallback = instance.CreateDebugReportCallback(DebugReportDelegate, DebugReportFlags.Error | DebugReportFlags.Warning | DebugReportFlags.PerformanceWarning);
+
+            //foreach (var extension in SharpVk.Instance.EnumerateExtensionProperties())
+            //    Console.WriteLine($"Extension available: {extension.ExtensionName}");
+            //
+            //foreach (var layer in SharpVk.Instance.EnumerateLayerProperties())
+            //    Console.WriteLine($"Layer available: {layer.LayerName}, {layer.Description}");
+
+            return (instance, debugReportCallback);
+        }
+
+        public void InitVulkan()
+        {
+            lock (Lock)
+            {
+                Window.CreateSurface();
+                DeviceComponent.CreateDevice();
+                SwapChainComponent.CreateSwapChain();
+                SwapChainComponent.CreateImageViews();
+                GraphicsComponent.CreateRenderPass();
+                GraphicsComponent.CreateDescriptorSetLayout();
+                ShaderComponent.CreateShaderModules();
+                GraphicsComponent.CreateGraphicsPipeline();
+                DeviceComponent.CreateCommandPools();
+                ImageComponent.CreateDepthResources();
+                SwapChainComponent.CreateFrameBuffers();
+                TextureComponent.CreateLogo();
+                foreach (var entity in Entities)
+                {
+                    entity.RenewAble.Create(this);
+                    AEnittyComponent.Entities.Add(entity);
+                }
+                ShaderComponent.CreateUniformBuffer();
+                GraphicsComponent.CreateDescriptorPool();
+                GraphicsComponent.CreateDescriptorSet();
+                DeviceComponent.CreateCommandBuffers();
+                SemaphoreComponent.CreateSemaphores();
+            }
+        }
+
+        public List<AEntity> Entities = new();
+
+        public void MainLoop(TimeSpan maxValue)
+        {
+            InitialTimestamp = Stopwatch.GetTimestamp();
+            Runing = true;
+
+            Window.MainLoop(maxValue);
+        }
     }
 }
--- ajiva/ProgramVulcanInit.cs
+++ ajiva/ProgramVulcanInit.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using ajiva.Engine;
 using SharpVk;
 using SharpVk.Multivendor;
 
@@ -7,74 +10,6 @@ namespace ajiva
 {
     public partial class Program
     {
-        private static (Instance instance, DebugReportCallback debugReportCallback) CreateInstance(IEnumerable<string> enabledExtensionNames)
-        {
-            //if (Instance != null) return;
 
-            List<string> enabledLayers = new();
-
-            var props = Instance.EnumerateLayerProperties();
-
-            void AddAvailableLayer(string layerName)
-            {
-                if (props.Any(x => x.LayerName == layerName))
-                    enabledLayers.Add(layerName);
-            }
-
-            AddAvailableLayer("VK_LAYER_LUNARG_standard_validation");
-            AddAvailableLayer("VK_LAYER_KHRONOS_validation");
-            AddAvailableLayer("VK_LAYER_GOOGLE_unique_objects");
-            //AddAvailableLayer("VK_LAYER_LUNARG_api_dump");
-            AddAvailableLayer("VK_LAYER_LUNARG_core_validation");
-            AddAvailableLayer("VK_LAYER_LUNARG_image");
-            AddAvailableLayer("VK_LAYER_LUNARG_object_tracker");
-            AddAvailableLayer("VK_LAYER_LUNARG_parameter_validation");
-            AddAvailableLayer("VK_LAYER_LUNARG_swapchain");
-            AddAvailableLayer("VK_LAYER_GOOGLE_threading");
-
-            var instance = Instance.Create(
-                enabledLayers.ToArray(),
-                enabledExtensionNames.Append(ExtExtensions.DebugReport).ToArray(),
-                applicationInfo: new ApplicationInfo
-                {
-                    ApplicationName = "ajiva",
-                    ApplicationVersion = new(0, 0, 1),
-                    EngineName = "ajiva-engine",
-                    EngineVersion = new(0, 0, 1),
-                    ApiVersion = new(1, 0, 0)
-                });
-
-            var debugReportCallback = instance.CreateDebugReportCallback(DebugReportDelegate, DebugReportFlags.Error | DebugReportFlags.Warning | DebugReportFlags.PerformanceWarning);
-            
-            //foreach (var extension in SharpVk.Instance.EnumerateExtensionProperties())
-            //    Console.WriteLine($"Extension available: {extension.ExtensionName}");
-            //
-            //foreach (var layer in SharpVk.Instance.EnumerateLayerProperties())
-            //    Console.WriteLine($"Layer available: {layer.LayerName}, {layer.Description}");
-
-            return (instance,debugReportCallback);
-        }
-
-        private void InitVulkan()
-        {
-            Window.CreateSurface();
-            DeviceManager.CreateDevice();
-            SwapChainManager.CreateSwapChain();
-            SwapChainManager.CreateImageViews();
-            GraphicsManager.CreateRenderPass();
-            GraphicsManager.CreateDescriptorSetLayout();
-            ShaderManager.CreateShaderModules();
-            GraphicsManager.CreateGraphicsPipeline();
-            DeviceManager.CreateCommandPools();
-            ImageManager.CreateDepthResources();
-            SwapChainManager.CreateFrameBuffers();
-            TextureManager.CreateLogo();
-            BufferManager.AddBuffer(vertices, indices);
-            ShaderManager.CreateUniformBuffer();
-            GraphicsManager.CreateDescriptorPool();
-            GraphicsManager.CreateDescriptorSet();
-            DeviceManager.CreateCommandBuffers();
-            SemaphoreManager.CreateSemaphores();
-        }
     }
 }
