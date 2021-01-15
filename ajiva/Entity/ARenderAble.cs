12295a3b725b2618deb3ce5622b988cd85f01c28
xuri02 <t0bl4s.durst@gmail.com>
2021-01-15 18:11:01 +0100

    binding description set with offset according to the render id

--- ajiva/Entity/ARenderAble.cs
+++ ajiva/Entity/ARenderAble.cs
@@ -9,14 +9,30 @@ namespace ajiva.Entity
 {
     public class ARenderAble : DisposingLogger
     {
+        public uint Id { get; protected set; }
         private IRenderEngine? render;
         public Mesh? Mesh { get; private set; }
+        public bool Render { get; set; } = true;
+        public const int DoNotRenderId = -1;
+
+        private static int currentMaxId = 0;
 
         public static int NextId() => currentMaxId++;
 
         public ARenderAble(Mesh? mesh, int id)
         {
             Mesh = mesh;
+            if (id >= 0)
+            {
+                Id = (uint)id;
+                Render = true;
+                Console.WriteLine($"Creating ARenderAble with id {Id}");
+            }
+            else
+            {
+                Render = false;
+                Console.WriteLine("Creating ARenderAble but nor Rendering");
+            }
         }
 
         public void BindAndDraw(CommandBuffer commandBuffer)
@@ -25,8 +41,8 @@ namespace ajiva.Entity
             ATrace.Assert(render != null, nameof(render) + " != null");
 
             Mesh.Bind(commandBuffer);
-            
-            commandBuffer.BindDescriptorSets(PipelineBindPoint.Graphics, render.GraphicsComponent.PipelineLayout, 0, render.GraphicsComponent.DescriptorSet, null);
+
+            commandBuffer.BindDescriptorSets(PipelineBindPoint.Graphics, render.GraphicsComponent.PipelineLayout, 0, render.GraphicsComponent.DescriptorSet, Id * (uint)Unsafe.SizeOf<UniformModel>());
 
             Mesh.DrawIndexed(commandBuffer);
         }
@@ -40,6 +56,7 @@ namespace ajiva.Entity
         /// <inheritdoc />
         protected override void ReleaseUnmanagedResources()
         {
+            currentMaxId--;
             Mesh?.Dispose();
         }
     }
