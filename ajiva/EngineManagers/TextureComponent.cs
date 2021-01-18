6ac70e249a24d3930fe75561373e0c66445354a8
xuri02 <t0bl4s.durst@gmail.com>
2021-01-18 22:12:51 +0100

    removed unused stuff from Texture manager because it was moved to TextureStatic

--- ajiva/EngineManagers/TextureComponent.cs
+++ ajiva/EngineManagers/TextureComponent.cs
@@ -24,50 +24,7 @@ namespace ajiva.EngineManagers
         private List<Texture> Textures { get; }
         public DescriptorImageInfo[] TextureSamplerImageViews { get; }
 
-        private AImage CreateTextureImageFromFile(string fileName)
-        {
-            var img = System.Drawing.Image.FromFile(fileName);
-            var bm = new Bitmap(img);
 
-            var texWidth = (uint)bm.Width;
-            var texHeight = (uint)bm.Height;
-            var imageSize = texWidth * texHeight * 4u;
-
-            using ABuffer aBuffer = new(imageSize);
-            aBuffer.Create(RenderEngine.DeviceComponent, BufferUsageFlags.TransferSource, MemoryPropertyFlags.HostVisible | MemoryPropertyFlags.HostCached);
-
-            unsafe
-            {
-                var scp0 = bm.LockBits(new(0, 0, bm.Width, bm.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppPArgb);
-
-                using (var map = aBuffer.MapDisposer())
-                {
-                    ImageHelper.ArgbCopyMap((ImageHelper.Argb32R*)scp0.Scan0.ToPointer(), (ImageHelper.Rgba32*)map.ToPointer(), texWidth * texHeight);
-                    //ImageHelper.ArgbCopyMap((byte*)scp0.Scan0.ToPointer(), (byte*)map.ToPointer(), texWidth * texHeight);
-                    //System.Buffer.MemoryCopy(scp0.Scan0.ToPointer(), map.ToPointer(), imageSize, imageSize);
-
-                    bm.UnlockBits(scp0);
-                }
-            }
-
-            AImage aImage = RenderEngine.ImageComponent.CreateImageAndView(texWidth, texHeight, Format.R8G8B8A8Srgb, ImageTiling.Optimal, ImageUsageFlags.TransferDestination | ImageUsageFlags.Sampled, MemoryPropertyFlags.DeviceLocal, ImageAspectFlags.Color);
-
-            RenderEngine.ImageComponent.TransitionImageLayout(aImage.Image, Format.R8G8B8A8Srgb, ImageLayout.Undefined, ImageLayout.TransferDestinationOptimal);
-            RenderEngine.ImageComponent.CopyBufferToImage(aBuffer.Buffer!, aImage.Image, texWidth, texHeight);
-            RenderEngine.ImageComponent.TransitionImageLayout(aImage.Image, Format.R8G8B8A8Srgb, ImageLayout.TransferDestinationOptimal, ImageLayout.ShaderReadOnlyOptimal);
-
-            return aImage;
-        }
-
-        private Sampler CreateTextureSampler()
-        {
-            var properties = RenderEngine.DeviceComponent.PhysicalDevice!.GetProperties();
-
-            var textureSampler = RenderEngine.DeviceComponent.Device!.CreateSampler(Filter.Linear, Filter.Linear, SamplerMipmapMode.Linear, SamplerAddressMode.Repeat,
-                SamplerAddressMode.Repeat, SamplerAddressMode.Repeat, default, true, properties.Limits.MaxSamplerAnisotropy,
-                false, CompareOp.Always, default, default, BorderColor.IntOpaqueBlack, false);
-            return textureSampler;
-        }
 
         public void AddAndMapTextureToDescriptor(Texture texture)
         {
@@ -99,12 +56,9 @@ namespace ajiva.EngineManagers
         {
             if (Default != null) return;
             RenderEngine.DeviceComponent.EnsureDevicesExist();
-            
-            Default = new(0)
-            {
-                Image = CreateTextureImageFromFile("logo.png"),
-                Sampler = CreateTextureSampler()
-            };
+
+            Default = Texture.FromFile(RenderEngine,"logo.png");
+
             for (var i = 0; i < MAX_TEXTURE_SAMPLERS_IN_SHADER; i++)
             {
                 TextureSamplerImageViews[i] = Default.DescriptorImageInfo;
