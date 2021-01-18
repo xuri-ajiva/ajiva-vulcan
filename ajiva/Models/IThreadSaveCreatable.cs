295304d72dd70e8e418512fcd867be97f33134e3
xuri02 <t0bl4s.durst@gmail.com>
2021-01-18 14:26:33 +0100

    adding IThreadSaveCreatable to Force Synchronized Creation of Unmanaged Resources

---
+++ ajiva/Models/IThreadSaveCreatable.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using ajiva.Engine;
+
+namespace ajiva.Models
+{
+    public interface IThreadSaveCreatable
+    {
+        bool Created { get; }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public abstract void EnsureExists();
+    }
+}
