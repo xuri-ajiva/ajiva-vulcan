804c43a1edb7e88b20f3beef9fdd2eb6d910a3aa
xuri02 <t0bl4s.durst@gmail.com>
2021-01-22 15:18:43 +0100

    moved BufferOfT to its own file

--- ajiva/Models/ABuffer.cs
+++ ajiva/Models/ABuffer.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
-using ajiva.Engine;
-using ajiva.EngineManagers;
 using ajiva.Helpers;
+using ajiva.Systems.RenderEngine.EngineManagers;
 using SharpVk;
 using Buffer = SharpVk.Buffer;
 
@@ -76,32 +74,6 @@ namespace ajiva.Models
             {
                 return Ptr.ToPointer();
             }
-        }
-    }
-    public class BufferOfT<T> : ABuffer where T : notnull
-    {
-        public int Length { get; }
-        public uint SizeOfT { get; }
-        public T[] Value { get; protected set; }
-
-        /// <inheritdoc />
-        public BufferOfT(T[] val) : base((uint)(Unsafe.SizeOf<T>() * val.Length))
-        {
-            Value = val;
-            Length = val.Length;
-            SizeOfT = (uint)Unsafe.SizeOf<T>();
-        }
-
-        public void ClearT()
-        {
-            Value = Array.Empty<T>();
-        }
-
-        /// <inheritdoc />
-        protected override void ReleaseUnmanagedResources()
-        {
-            ClearT();
-            base.ReleaseUnmanagedResources();
         }
     }
 }
---
+++ ajiva/Models/BufferOfT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ajiva.Models
+{
+    public class BufferOfT<T> : ABuffer where T : notnull
+    {
+        public int Length { get; }
+        public uint SizeOfT { get; }
+        public T[] Value { get; protected set; }
+
+        /// <inheritdoc />
+        public BufferOfT(T[] val) : base((uint)(Unsafe.SizeOf<T>() * val.Length))
+        {
+            Value = val;
+            Length = val.Length;
+            SizeOfT = (uint)Unsafe.SizeOf<T>();
+        }
+
+        public void ClearT()
+        {
+            Value = Array.Empty<T>();
+        }
+
+        /// <inheritdoc />
+        protected override void ReleaseUnmanagedResources()
+        {
+            ClearT();
+            base.ReleaseUnmanagedResources();
+        }
+    }
+}
