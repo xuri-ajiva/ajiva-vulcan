5ea387e278b4936e6e122997ee9959b838850b4f
xuri02 <t0bl4s.durst@gmail.com>
2021-01-22 19:37:41 +0100

    moved AjivaRenderEngine Fields into different file

--- ajiva/Systems/AjivaRenderEngine.cs
+++ ajiva/Systems/AjivaRenderEngine.cs
@@ -21,61 +21,9 @@ namespace ajiva.Systems.RenderEngine
 {
     public partial class AjivaRenderEngine : ComponentSystemBase<ARenderAble>, IRenderEngine, IDisposable
     {
-        public AjivaRenderEngine(Instance instance)
-        {
-            Instance = instance;
-            DeviceComponent = new(this);
-            SwapChainComponent = new(this);
-            ImageComponent = new(this);
-            Window = new(this);
-            GraphicsComponent = new(this);
-            ShaderComponent = new(this);
-            SemaphoreComponent = new(this);
-            TextureComponent = new(this);
-        }
-
         //public static Instance? Instance { get; set; }
         /// <inheritdoc />
         public bool Runing { get; set; }
-
-        /// <inheritdoc />
-        public Instance? Instance { get; set; }
-
-        /// <inheritdoc />
-        public DeviceComponent DeviceComponent { get; }
-
-        /// <inheritdoc />
-        public SwapChainComponent SwapChainComponent { get; }
-
-        /// <inheritdoc />
-        public PlatformWindow Window { get; }
-
-        /// <inheritdoc />
-        public ImageComponent ImageComponent { get; }
-
-        /// <inheritdoc />
-        public GraphicsComponent GraphicsComponent { get; }
-
-        /// <inheritdoc />
-        public ShaderComponent ShaderComponent { get; }
-
-        /// <inheritdoc />
-        public SemaphoreComponent SemaphoreComponent { get; }
-
-        /// <inheritdoc />
-        public TextureComponent TextureComponent { get; }
-
-        /// <inheritdoc />
-        public event PlatformEventHandler OnFrame;
-
-        /// <inheritdoc />
-        public event KeyEventHandler OnKeyEvent;
-
-        /// <inheritdoc />
-        public event EventHandler OnResize;
-
-        /// <inheritdoc />
-        public event EventHandler<vec2> OnMouseMove;
 
         /// <inheritdoc />
         public Cameras.Camera MainCamara
@@ -87,12 +35,6 @@ namespace ajiva.Systems.RenderEngine
                 mainCamara = value;
             }
         }
-
-        /// <inheritdoc />
-        public object RenderLock { get; } = new();
-
-        /// <inheritdoc />
-        public object UpdateLock { get; } = new();
 
         /// <inheritdoc />
         public AjivaEcs Ecs { get; set; }
@@ -190,7 +132,7 @@ namespace ajiva.Systems.RenderEngine
             {
                 CommandBuffers = new[]
                 {
-                    DeviceComponent.CommandBuffers![nextImage]
+                    GraphicsComponent.Current.CommandBuffers![nextImage]
                 },
                 SignalSemaphores = new[]
                 {
---
+++ ajiva/Systems/RenderEngine/AjivaRenderEngine.Fields.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using ajiva.Systems.RenderEngine.EngineManagers;
+using GlmSharp;
+using SharpVk;
+
+namespace ajiva.Systems.RenderEngine
+{
+    public partial class AjivaRenderEngine
+    {
+        public AjivaRenderEngine(Instance instance)
+        {
+            Instance = instance;
+            DeviceComponent = new(this);
+            SwapChainComponent = new(this);
+            ImageComponent = new(this);
+            Window = new(this);
+            GraphicsComponent = new(this);
+            ShaderComponent = new(this);
+            SemaphoreComponent = new(this);
+            TextureComponent = new(this);
+        }
+
+        /// <inheritdoc />
+        public Instance? Instance { get; set; }
+
+        /// <inheritdoc />
+        public DeviceComponent DeviceComponent { get; }
+
+        /// <inheritdoc />
+        public SwapChainComponent SwapChainComponent { get; }
+
+        /// <inheritdoc />
+        public PlatformWindow Window { get; }
+
+        /// <inheritdoc />
+        public ImageComponent ImageComponent { get; }
+
+        /// <inheritdoc />
+        public GraphicsComponent GraphicsComponent { get; }
+
+        /// <inheritdoc />
+        public ShaderComponent ShaderComponent { get; }
+
+        /// <inheritdoc />
+        public SemaphoreComponent SemaphoreComponent { get; }
+
+        /// <inheritdoc />
+        public TextureComponent TextureComponent { get; }
+
+        /// <inheritdoc />
+        public event PlatformEventHandler OnFrame;
+
+        /// <inheritdoc />
+        public event KeyEventHandler OnKeyEvent;
+
+        /// <inheritdoc />
+        public event EventHandler OnResize;
+
+        /// <inheritdoc />
+        public event EventHandler<vec2> OnMouseMove;
+
+        /// <inheritdoc />
+        public object RenderLock { get; } = new();
+
+        /// <inheritdoc />
+        public object UpdateLock { get; } = new();
+    }
+}
