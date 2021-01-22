f2c4b2ffcd5726126f297846cfa988ada213f860
xuri02 <t0bl4s.durst@gmail.com>
2021-01-22 15:30:33 +0100

    adding unused Transform Entity Factory

---
+++ ajiva/Factorys/TransformEntityFactory.cs
@@ -0,0 +1,23 @@
+﻿using ajiva.Components;
+using ajiva.Ecs;
+using ajiva.Ecs.Factory;
+using ajiva.Entitys;
+
+namespace ajiva.Factorys
+{
+    public class TransformEntityFactory : EntityFactoryBase<TransFormEntity>
+    {
+        /// <inheritdoc />
+        public override TransFormEntity Create(AjivaEcs system, uint id)
+        {
+            var entity = new TransFormEntity() {Id = id};
+            system.AttachComponentToEntity<Transform3d>(entity);
+            return entity;
+        }
+
+        /// <inheritdoc />
+        protected override void ReleaseUnmanagedResources()
+        {
+        }
+    }
+}
