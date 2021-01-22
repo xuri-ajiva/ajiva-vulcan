d2b96d36f888c10984d478f64c95ae64dccbb1e5
xuri02 <t0bl4s.durst@gmail.com>
2021-01-22 15:27:39 +0100

    marking ARenderAble an Component and removing BindAndDraw should not be hear

---
+++ ajiva/Components/ARenderAble.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using ajiva.Ecs.Component;
+using ajiva.Helpers;
+using ajiva.Models;
+using ajiva.Systems.RenderEngine.EngineManagers;
+
+namespace ajiva.Components
+{
+    public class ARenderAble : DisposingLogger, IComponent
+    {
+        public uint Id { get; }
+        public Mesh? Mesh { get; private set; }
+        public bool Render { get; set; }
+
+        public ARenderAble()
+        {
+            Render = false;
+            Id = NextId();
+        }
+        
+        public void SetMesh(Mesh? mesh, DeviceComponent component)
+        {
+            Mesh?.Dispose();
+            Mesh = mesh;
+            Mesh?.Create(component);
+            Render &= mesh != null;
+        }
+
+        /// <inheritdoc />
+        protected override void ReleaseUnmanagedResources()
+        {
+            UsedIds.Remove(Id);
+            Mesh?.Dispose();
+        }
+
+        private static readonly HashSet<uint> UsedIds = new();
+
+        public static uint NextId()
+        {
+            for (uint i = 0;; i++)
+            {
+                if (UsedIds.Contains(i)) continue;
+                
+                UsedIds.Add(i);
+                return i;
+            }
+        }
+    }
+}
