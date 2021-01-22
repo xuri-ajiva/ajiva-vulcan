b18b90c8846d2f13b7c3c9df4bcf1242b8b7c84f
xuri02 <t0bl4s.durst@gmail.com>
2021-01-22 15:13:24 +0100

    created Default Entity

---
+++ ajiva/Entitys/DefaultEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ajiva.Ecs.Entity;
+
+namespace ajiva.Entitys
+{
+    public class DefaultEntity : AEntity
+    {
+        /// <inheritdoc />
+        protected override void ReleaseUnmanagedResources()
+        {
+        }
+
+        /// <inheritdoc />
+        public override void Update(TimeSpan delta)
+        {
+        }
+    }
+}
