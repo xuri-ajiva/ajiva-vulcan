f315329f698d9dc1abf12525a1bee129d345748c
xuri02 <t0bl4s.durst@gmail.com>
2021-01-22 15:07:38 +0100

    added IComponentSystem and ComponentSystemBase for eCs

---
+++ ajiva/Ecs/Component/ComponentSystemBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ajiva.Ecs.Entity;
+using ajiva.Helpers;
+
+namespace ajiva.Ecs.Component
+{
+    public abstract class ComponentSystemBase<T> : DisposingLogger, IComponentSystem<T> where T : class, IComponent
+    {
+        public Type ComponentType { get; } = typeof(T);
+
+        public Dictionary<T, IEntity> ComponentEntityMap { get; } = new();
+
+        /// <inheritdoc />
+        public abstract void Update(TimeSpan delta);
+
+        public abstract Task Init(AjivaEcs ecs);
+
+        /// <inheritdoc />
+        object IComponentSystem.CreateComponent(IEntity entity) => CreateComponent(entity);
+
+        /// <inheritdoc />
+        public abstract void AttachNewComponent(IEntity entity);
+
+        /// <inheritdoc />
+        public abstract T CreateComponent(IEntity entity);
+
+        /// <inheritdoc />
+        object IComponentSystem.ComponentEntityMap => ComponentEntityMap;
+    }
+}
