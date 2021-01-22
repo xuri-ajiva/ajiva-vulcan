3ef8f0755483c1ff1d1d20af5323c649915b2c7b
xuri02 <t0bl4s.durst@gmail.com>
2021-01-22 15:15:08 +0100

    created Transform Entity for having an Transform as Lazy

---
+++ ajiva/Entitys/TransFormEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ajiva.Components;
+
+namespace ajiva.Entitys
+{
+    public class TransFormEntity : DefaultEntity
+    {
+        public TransFormEntity()
+        {
+            TransformLazy = new(GetComponent<Transform3d>);
+        }
+
+        public Lazy<Transform3d> TransformLazy { get; }
+        public Transform3d Transform => TransformLazy.Value;
+
+        /// <inheritdoc />
+        public override void Update(TimeSpan delta)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void ReleaseUnmanagedResources()
+        {
+        }
+    }
+}
