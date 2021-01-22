40e833daa45d64394999df2dfc259d7a6a7f42ea
xuri02 <t0bl4s.durst@gmail.com>
2021-01-22 15:07:09 +0100

    added AEntity/IEntity for Ecs

---
+++ ajiva/Ecs/Entity/AEntity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using ajiva.Ecs.Component;
+using ajiva.Helpers;
+
+namespace ajiva.Ecs.Entity
+{
+    public abstract class AEntity : DisposingLogger, IEntity
+    {
+        public static uint CurrentId { get; [MethodImpl(MethodImplOptions.Synchronized)] private set; }
+
+        /// <inheritdoc />
+        public uint Id { get; init; } = CurrentId++;
+
+        /// <inheritdoc />
+        public IDictionary<Type, IComponent> Components { get; } = new Dictionary<Type, IComponent>();
+
+        /// <inheritdoc />
+        public bool TryGetComponent<T>(out T? value) where T : class, IComponent
+        {
+            if (Components.TryGetValue(typeof(T), out var tmp))
+            {
+                value = (T)tmp;
+                return true;
+            }
+            value = default;
+            return false;
+
+            /*
+            foreach (var component in Components)
+                if (component is T tmp)
+                {
+                    value = tmp;
+                    return true;
+                }
+            value = default;
+            return false;*/
+        }
+
+        public T GetComponent<T>() where T : class, IComponent
+        {
+            return (T)Components[typeof(T)]; /*
+            foreach (var component in Components)
+                if (component is T c)
+                    return c;
+            return default;*/
+        }
+
+        /// <inheritdoc />
+        public bool HasComponent<T>() where T : class, IComponent
+        {
+            return Components.ContainsKey(typeof(T));
+            //return Components.Any(c => c.GetType() == typeof(T));
+        }
+
+        //public void AddComponent<T>(T component) where T : class, IComponent
+        //{
+        //    Components.Add(typeof(T), component);
+        //}
+
+        public bool HasUpdate { get; protected set; }
+        public abstract void Update(TimeSpan delta);
+    }
+}
---
+++ ajiva/Ecs/Entity/IEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using ajiva.Ecs.Component;
+
+namespace ajiva.Ecs.Entity
+{
+    public interface IEntity: IDisposable
+    {
+        uint Id { get; init; }
+
+        IDictionary<Type, IComponent> Components { get; }
+
+        bool TryGetComponent<T>(out T? value) where T : class, IComponent;
+        T? GetComponent<T>() where T : class, IComponent;
+        bool HasComponent<T>() where T : class, IComponent;
+        bool HasUpdate { get; }
+        void Update(TimeSpan delta);
+    }
+}
