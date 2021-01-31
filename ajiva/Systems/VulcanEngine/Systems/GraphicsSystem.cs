05ff864d735307a12d7dfb6ceb7e6b2353dcd69c
xuri02 <t0bl4s.durst@gmail.com>
2021-01-31 14:46:52 +0100

    GraphicsComponent -> GraphicsSystem

---
+++ ajiva/Systems/VulcanEngine/Systems/GraphicsSystem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using ajiva.Ecs;
+using ajiva.Ecs.System;
+using ajiva.Systems.VulcanEngine.EngineManagers;
+
+namespace ajiva.Systems.VulcanEngine.Systems
+{
+    public class GraphicsSystem : SystemBase, IInit, IUpdate
+    {
+        public static readonly object CurrentGraphicsLayoutSwapLock = new();
+        public GraphicsLayout? Current { get; private set; }
+
+        /// <inheritdoc />
+        protected override void ReleaseUnmanagedResources()
+        {
+            EnsureGraphicsLayoutDeletion();
+        }
+
+        public void EnsureGraphicsLayoutExists()
+        {
+            Current ??= new(Ecs);
+        }
+
+        public void EnsureGraphicsLayoutDeletion()
+        {
+            Current?.DisposeIn(1000);
+            Current = null;
+        }
+
+        public void RecreateCurrentGraphicsLayoutAsync()
+        {
+            new Thread(() =>
+            {
+                var created = new GraphicsLayout(Ecs);
+                created.EnsureExists();
+                GraphicsLayout? old;
+                lock (CurrentGraphicsLayoutSwapLock)
+                {
+                    old = Current;
+                    Current = created;
+                }
+                Thread.Sleep(1000);
+                old?.Dispose();
+            }).Start();
+        }
+
+        public void RecreateCurrentGraphicsLayout()
+        {
+            var created = new GraphicsLayout(Ecs);
+            created.EnsureExists();
+            GraphicsLayout? old;
+            lock (CurrentGraphicsLayoutSwapLock)
+            {
+                old = Current;
+                Current = created;
+            }
+
+            old?.DisposeIn(1000);
+        }
+
+        /// <inheritdoc />
+        protected override void Setup()
+        {
+            Ecs.RegisterInit(this, InitPhase.PostMain);
+            Ecs.RegisterUpdate(this);
+        }
+
+        /// <inheritdoc />
+        public void Init(AjivaEcs ecs, InitPhase phase)
+        {
+            RecreateCurrentGraphicsLayout();
+        }
+
+        /// <inheritdoc />
+        public void Update(TimeSpan delta)
+        {
+            //Console.WriteLine("Frame with " + Current?.GetHashCode());
+            Current?.DrawFrame();
+        }
+    }
+}
