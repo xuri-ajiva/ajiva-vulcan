93a39a07f2a5267e0f453cccbe9a97ad216066c6
xuri02 <t0bl4s.durst@gmail.com>
2021-02-01 15:31:48 +0100

    added Texture Generator

---
+++ ajiva/Generators/Texture/BoxTextureGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using ajiva.Components;
+using ajiva.Ecs;
+using ajiva.Ecs.System;
+using ajiva.Systems.VulcanEngine.Systems;
+using ajiva.Worker;
+using SharpVk;
+
+namespace ajiva.Generators.Texture
+{
+    public class BoxTextureGenerator : SystemBase, IInit
+    {
+        public ATexture MissingTexture { get; private set; }
+
+        /// <inheritdoc />
+        protected override void ReleaseUnmanagedResources()
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void Setup()
+        {
+            Ecs.RegisterInit(this, InitPhase.Post);
+        }
+
+        /// <inheritdoc />
+        public void Init(AjivaEcs ecs, InitPhase phase)
+        {
+            Ecs.GetSystem<WorkerPool>().EnqueueWork(delegate
+            {
+                var bitmap = new Bitmap(4048, 4048);
+
+                var g = Graphics.FromImage(bitmap);
+
+                g.DrawRectangle(Pens.Black, 0, 0, bitmap.Height, bitmap.Width);
+
+                g.DrawString("Missing\nTexture", new(FontFamily.GenericMonospace, 600, FontStyle.Bold, GraphicsUnit.Pixel), new SolidBrush(Color.White), new PointF(600,600));
+
+                g.Flush();
+
+                MissingTexture = ATexture.FromBitmap(Ecs, bitmap);
+                //MissingTexture.TextureId = 0;
+
+                Ecs.GetComponentSystem<TextureSystem, ATexture>().AddAndMapTextureToDescriptor(MissingTexture);
+                
+                return WorkResult.Succeeded;
+            }, Console.WriteLine, "Missing Texture Generator");
+
+            //Ecs.GetSystem<WorkerPool>().;
+        }
+    }
+}
