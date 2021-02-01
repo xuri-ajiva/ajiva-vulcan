66286ac6ab5de41a130858bc4dbd0a6678ff1063
xuri02 <t0bl4s.durst@gmail.com>
2021-02-01 15:30:33 +0100

    added ConsoleHelper

---
+++ ajiva/Helpers/ConsoleExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ajiva.Helpers
+{
+    public static class ConsoleExtensions
+    {
+        public static string FillUp(this string toPad, int length)
+        {
+            //toPad += new string(Enumerable.Repeat(' ', length - toPad.Length).ToArray());
+            //return toPad;
+            while (toPad.Length < length)
+            {
+                toPad += ' ';
+            }
+            return toPad;
+        }
+    }
+}
