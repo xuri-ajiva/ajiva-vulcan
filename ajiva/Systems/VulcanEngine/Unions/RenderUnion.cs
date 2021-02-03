8af88edb805ab38f8017320425c4700c0f28c88c
xuri02 <t0bl4s.durst@gmail.com>
2021-02-03 19:39:50 +0100

    adding render union for filling command buffers and drawing

---
+++ ajiva/Systems/VulcanEngine/Unions/RenderUnion.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using ajiva.Components;
+using ajiva.Helpers;
+using ajiva.Models;
+using ajiva.Systems.VulcanEngine.Systems;
+using SharpVk;
+using SharpVk.Khronos;
+
+namespace ajiva.Systems.VulcanEngine.Unions
+{
+    public class RenderUnion : DisposingLogger
+    {
+        public readonly SwapChainUnion swapChainUnion;
+        public readonly Dictionary<PipelineName, PipelineFrameUnion> Unions;
+
+        public Semaphore ImageAvailable { get; }
+        public Semaphore RenderFinished { get; }
+
+        /// <inheritdoc />
+        public RenderUnion(SwapChainUnion swapChainUnion, Semaphore imageAvailable, Semaphore renderFinished, Dictionary<PipelineName, PipelineFrameUnion> unions)
+        {
+            this.swapChainUnion = swapChainUnion;
+            Unions = unions;
+
+            ImageAvailable = imageAvailable;
+            RenderFinished = renderFinished;
+        }
+
+        public static RenderUnion CreateRenderUnion(PhysicalDevice physicalDevice, Device device, Surface surface, Extent2D surfaceExtent, ShaderSystem system, DescriptorImageInfo[] textureSamplerImageViews, bool useDepthImage, AImage depthImage, CommandPool commandPool)
+        {
+            var swap = SwapChainUnion.CreateSwapChainUnion(physicalDevice, device, surface, surfaceExtent);
+            var graph3d = GraphicsPipelineUnion.CreateGraphicsPipelineUnion3D(swap, physicalDevice, device, system, textureSamplerImageViews);
+            var graph2d = GraphicsPipelineUnion.CreateGraphicsPipelineUnion2D(swap, physicalDevice, device, system, textureSamplerImageViews);
+            var frame3d = FrameBufferUnion.CreateFrameBufferUnion(swap, graph3d, device, useDepthImage, depthImage, commandPool);
+            var frame2d = FrameBufferUnion.CreateFrameBufferUnion(swap, graph2d, device, false, depthImage, commandPool);
+
+            var imageAvailable = device.CreateSemaphore()!;
+            var renderFinished = device.CreateSemaphore()!;
+
+            return new(swap, imageAvailable, renderFinished, new()
+            {
+                [PipelineName.PipeLine3d] = new(graph3d, frame3d),
+                [PipelineName.PipeLine2d] = new(graph2d, frame2d),
+            });
+        }
+
+        private void FillBuffer(CommandBuffer buffer, Framebuffer framebuffer, GraphicsPipelineUnion graphicsPipelineUnion, IEnumerable<ARenderAble> renders, ClearValue[] clearValues)
+        {
+            buffer.Reset();
+
+            buffer.Begin(CommandBufferUsageFlags.SimultaneousUse);
+
+            buffer.BeginRenderPass(graphicsPipelineUnion.RenderPass,
+                framebuffer,
+                new(new(), swapChainUnion.SwapChainExtent),
+                clearValues,
+                SubpassContents.Inline);
+
+            buffer.BindPipeline(PipelineBindPoint.Graphics, graphicsPipelineUnion.Pipeline);
+
+            foreach (var renderAble in renders)
+            {
+                if (!renderAble.Render) continue;
+                buffer.BindDescriptorSets(PipelineBindPoint.Graphics, graphicsPipelineUnion.PipelineLayout, 0, graphicsPipelineUnion.DescriptorSet, renderAble.Id * (uint)Unsafe.SizeOf<UniformModel>());
+                renderAble.BindAndDraw(buffer);
+            }
+
+            buffer.EndRenderPass();
+            buffer.End();
+        }
+
+        public void FillFrameBuffers(Dictionary<PipelineName, List<ARenderAble>> render)
+        {
+            lock (bufferLock)
+            {
+                foreach (var (pipelineName, graphicsFrameUnion) in Unions)
+                {
+                    if (!render.ContainsKey(pipelineName)) render.Add(pipelineName, new());
+
+                    var clear = pipelineName switch
+                    {
+                        PipelineName.PipeLine3d => new ClearValue[]
+                        {
+                            new ClearColorValue(.1f, .1f, .1f, .1f),
+                            new ClearDepthStencilValue(1, 0),
+                        },
+                        PipelineName.PipeLine2d => Array.Empty<ClearValue>(),
+                        _ => Array.Empty<ClearValue>(),
+                    };
+
+                    for (var index = 0; index < graphicsFrameUnion.FrameBuffer.FrameBuffers.Length; index++)
+                    {
+                        var commandBuffer = graphicsFrameUnion.FrameBuffer.RenderBuffers[index];
+                        var framebuffer = graphicsFrameUnion.FrameBuffer.FrameBuffers[index];
+                        FillBuffer(commandBuffer, framebuffer, graphicsFrameUnion.PipelineUnion, render[pipelineName], clear);
+                    }
+                }
+            }
+        }
+
+        public void FillFrameBuffers(IEnumerable<ARenderAble> renderAbles)
+        {
+            var render = renderAbles.Where(able => able.Render).GroupBy(able => able.PipelineName, able => able).ToDictionary(names => names.Key, names => names.ToList());
+
+            FillFrameBuffers(render);
+        }
+
+        private readonly object bufferLock = new();
+
+        public void DrawFrame(Queue graphicsQueue, Queue presentQueue)
+        {
+            lock (bufferLock)
+            {
+                if (Disposed) return;
+
+                CommandBuffer[] buffers = new CommandBuffer[Unions.Count];
+
+                var nextImage = swapChainUnion.SwapChain!.AcquireNextImage(uint.MaxValue, ImageAvailable, null);
+
+                var i = 0;
+                foreach (var union in Unions)
+                {
+                    buffers[i++] = union.Value.FrameBuffer.RenderBuffers[nextImage];
+                }
+
+                var si = new SubmitInfo
+                {
+                    CommandBuffers = buffers,
+                    SignalSemaphores = new[]
+                    {
+                        RenderFinished
+                    },
+                    WaitDestinationStageMask = new[]
+                    {
+                        PipelineStageFlags.ColorAttachmentOutput
+                    },
+                    WaitSemaphores = new[]
+                    {
+                        ImageAvailable
+                    }
+                };
+
+                graphicsQueue!.Submit(si, null);
+
+                var result = new Result[1];
+                presentQueue.Present(RenderFinished, swapChainUnion.SwapChain, nextImage, result);
+                si.SignalSemaphores = null!;
+                si.WaitSemaphores = null!;
+                si.WaitDestinationStageMask = null;
+                si.CommandBuffers = null;
+                // ReSharper disable once RedundantAssignment
+                result = null;
+                // ReSharper disable once RedundantAssignment
+                si = default;
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void ReleaseUnmanagedResources()
+        {
+            lock (bufferLock)
+            {
+                swapChainUnion.Dispose();
+                foreach (var (_, union) in Unions)
+                {
+                    union.Dispose();
+                }
+
+                ImageAvailable.Dispose();
+                RenderFinished.Dispose();
+            }
+        }
+    }
+    public enum PipelineName
+    {
+        PipeLine3d,
+        PipeLine2d,
+    }
+}
