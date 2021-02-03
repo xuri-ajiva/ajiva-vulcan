1e3a1e3ad31b5dd3c43dd66e4e908068717e665f
xuri02 <t0bl4s.durst@gmail.com>
2021-02-03 19:21:58 +0100

    adding ui renderer as ComponentSystemBase

---
+++ ajiva/Systems/VulcanEngine/Ui/UiRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ajiva.Components;
+using ajiva.Ecs;
+using ajiva.Ecs.ComponentSytem;
+using ajiva.Ecs.Entity;
+using ajiva.Systems.VulcanEngine.Engine;
+
+namespace ajiva.Systems.VulcanEngine.Ui
+{
+    public class UiRenderer : ComponentSystemBase<ARenderAble2D>
+    {
+        /// <inheritdoc />
+        protected override void Setup()
+        {
+        }
+
+        /// <inheritdoc />
+        public override void AttachNewComponent(IEntity entity)
+        {
+            entity.AddComponent(CreateComponent(entity));
+        }
+
+        /// <inheritdoc />
+        public override ARenderAble2D CreateComponent(IEntity entity)
+        {
+            var comp = new ARenderAble2D();
+            ComponentEntityMap.Add(comp, entity);
+            return comp;
+        }
+    }
+}
