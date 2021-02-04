9a0c5ebb1f981a1fab75edf7d498e5ffd57ca65c
xuri02 <t0bl4s.durst@gmail.com>
2021-02-04 16:40:56 +0100

    added Notify as ValueNotify option

---
+++ ajiva/Helpers/Notify.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace ajiva.Helpers
+{
+    public class Notify<T>
+    {
+        public event Action<T?, T?>? OnChanged;
+        public T? innerValue;
+        public T Value
+        {
+            get => innerValue;
+            set
+            {
+                if (value != null && !value.Equals(innerValue)) return;
+                Changed(innerValue, value);
+                innerValue = value;
+            }
+        }
+
+        public void Changed(T? oldValue, T? newValue)
+        {
+            OnChanged?.Invoke(oldValue, newValue);
+        }
+
+        public void Publish(T? newValue)
+        {
+            Changed(innerValue, newValue);
+            innerValue = newValue;
+        }
+
+        public void Subscribe(Action<T?,T?> action, CancellationToken cancellationToken)
+        {
+            cancellationToken.Register(() => OnChanged -= action);
+            OnChanged += action;
+        }
+    }
+}
