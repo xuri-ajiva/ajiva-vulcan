779a1ad9d9f5a9645e0fa40e338ef56ad5dc8eff
xuri02 <t0bl4s.durst@gmail.com>
2021-02-04 16:46:30 +0100

    added random ui rect pos

--- ajiva/Systems/VulcanEngine/Ui/UiRenderer.cs
+++ ajiva/Systems/VulcanEngine/Ui/UiRenderer.cs
@@ -1,14 +1,23 @@
-﻿using ajiva.Components;
+﻿using System;
+using System.Collections.Generic;
+using ajiva.Components;
+using ajiva.Ecs;
 using ajiva.Ecs.ComponentSytem;
 using ajiva.Ecs.Entity;
+using ajiva.Helpers;
+using ajiva.Models;
+using ajiva.Systems.VulcanEngine.Systems;
+using ajiva.Systems.VulcanEngine.Unions;
+using GlmSharp;
 
 namespace ajiva.Systems.VulcanEngine.Ui
 {
-    public class UiRenderer : ComponentSystemBase<ARenderAble2D>
+    public class UiRenderer : ComponentSystemBase<ARenderAble2D>, IUpdate
     {
         /// <inheritdoc />
         protected override void Setup()
         {
+            Ecs.RegisterUpdate(this);
         }
 
         /// <inheritdoc />
@@ -23,6 +32,30 @@ namespace ajiva.Systems.VulcanEngine.Ui
             var comp = new ARenderAble2D();
             ComponentEntityMap.Add(comp, entity);
             return comp;
+        }
+
+        private Random r = new();
+        /// <inheritdoc />
+        public void Update(UpdateInfo delta)
+        {
+            ShaderSystem shaderSystem = Ecs.GetSystem<ShaderSystem>();
+
+            var union = shaderSystem.ShaderUnions[PipelineName.PipeLine2d];
+
+            union.ViewProj.UpdateExpresion((int index, ref UniformViewProj value) =>
+            {
+                value.View = mat4.Identity;
+            });
+            union.ViewProj.Copy();
+
+            var ids = new List<uint>();
+            foreach (var entity in ComponentEntityMap.Keys)
+            {
+                ids.Add(entity.Id);
+                union.UniformModels.UpdateOne(new() {Model = mat4.Translate((float)r.NextDouble(),(float)r.NextDouble(),0)}, entity.Id);
+            }
+            
+            union.UniformModels.CopyRegions(ids);
         }
     }
 }
