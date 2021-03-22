81439d1b3c01d39f957c9d28f259a94481be7b62
xuri02 <t0bl4s.durst@gmail.com>
2021-03-22 15:53:10 +0100

    added Unique Static Value Cache to store Size and Type Key

---
+++ ajiva/Helpers/UsVc.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using ajiva.Ecs.Utils;
+
+namespace ajiva.Helpers
+{
+    //Unique Static Value Cache
+    public static class UsVc<T>
+    {
+        private static readonly int Size1;
+        private static readonly TypeKey Key1;
+
+        static UsVc()
+        {
+            Size1 = Unsafe.SizeOf<T>();
+            var hc = typeof(T).GetHashCode() ^ Size1;
+            for (var i = 0; i < 1000 && UsVc.KeySet.Contains(hc); i++)
+            {
+                hc = unchecked(hc ^ i + i);
+            }
+            UsVc.KeySet.Add(hc);
+            Key1 = (TypeKey)hc;
+        }
+
+        public static int Size => Size1;
+        public static TypeKey Key => Key1;
+    }
+    //only to make int into an type
+    public enum TypeKey : int
+    {
+    }
+
+    public static class UsVc
+    {
+        public static HashSet<int> KeySet { get; } = new();
+
+        public static TypeKey TypeKey<T>(T nb)
+        {
+            return UsVc<T>.Key;
+        }
+    }
+}
