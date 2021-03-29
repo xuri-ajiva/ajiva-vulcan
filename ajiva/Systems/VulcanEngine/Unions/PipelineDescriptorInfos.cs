fbe5e6d3ed76664408f896c81887eb2555afd603
xuri02 <t0bl4s.durst@gmail.com>
2021-03-29 17:37:57 +0200

    adding PipelineDescriptorInfos to reduce interpolation and repetition on descriptor set creation

---
+++ ajiva/Systems/VulcanEngine/Unions/PipelineDescriptorInfos.cs
@@ -0,0 +1,18 @@
+﻿using ajiva.Models.Buffer;
+using SharpVk;
+
+namespace ajiva.Systems.VulcanEngine.Unions
+{
+    public record PipelineDescriptorInfos(DescriptorType DescriptorType, ShaderStageFlags StageFlags, uint DestinationBinding, uint DescriptorCount, uint DestinationArrayElement = 0, DescriptorImageInfo[]? ImageInfo = default, BufferView[]? TexelBufferView = default, DescriptorBufferInfo[]? BufferInfo = default)
+    {
+        public static PipelineDescriptorInfos[] CreateFrom(UniformBuffer<UniformViewProj> viewProj, UniformBuffer<UniformModel> uniformModels, DescriptorImageInfo[] textureSamplerImageViews)
+        {
+            return new PipelineDescriptorInfos[]
+            {
+                new(DescriptorType.UniformBuffer, ShaderStageFlags.Vertex, 0, 1, BufferInfo: new[] {new DescriptorBufferInfo {Buffer = viewProj.Uniform.Buffer, Offset = 0, Range = viewProj.Uniform.SizeOfT}}),
+                new(DescriptorType.UniformBufferDynamic, ShaderStageFlags.Vertex, 1, 1, BufferInfo: new[] {new DescriptorBufferInfo {Buffer = uniformModels.Uniform.Buffer, Offset = 0, Range = uniformModels.Uniform.SizeOfT}}),
+                new(DescriptorType.CombinedImageSampler, ShaderStageFlags.Fragment, 2, (uint)textureSamplerImageViews.Length, ImageInfo: textureSamplerImageViews),
+            };
+        }
+    }
+}
