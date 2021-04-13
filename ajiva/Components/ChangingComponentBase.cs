762e051da740fb69158249e41f5912ef1fa66e4d
xuri02 <t0bl4s.durst@gmail.com>
2021-04-13 16:22:20 +0200

    added ChangingComponentBase as base for dynamic components

---
+++ ajiva/Components/ChangingComponentBase.cs
@@ -0,0 +1,19 @@
+﻿using ajiva.Ecs.Component;
+using ajiva.Models;
+using ajiva.Systems;
+using ajiva.Utils;
+using ajiva.Utils.Changing;
+
+namespace ajiva.Components
+{
+    public class ChangingComponentBase : DisposingLogger, IComponent
+    {
+        /// <inheritdoc />
+        public ChangingComponentBase(ChangingCacheMode mode)
+        {
+            ChangingObserver = new ChangingObserver(mode);
+        }
+
+        public IChangingObserver ChangingObserver { get; }
+    }
+}
