cbe050d3dac3435c1558a05798e33bd709876dd8
xuri02 <t0bl4s.durst@gmail.com>
2021-05-01 15:26:42 +0200

    kicked GraphicsLayout out of chain

--- ajiva/Systems/VulcanEngine/Systems/GraphicsSystem.cs
+++ ajiva/Systems/VulcanEngine/Systems/GraphicsSystem.cs
@@ -1,93 +1,113 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ajiva.Components.Media;
+using ajiva.Components.RenderAble;
 using ajiva.Ecs;
 using ajiva.Ecs.System;
 using ajiva.Ecs.Utils;
-using ajiva.Systems.VulcanEngine.EngineManagers;
 using ajiva.Systems.VulcanEngine.Ui;
+using ajiva.Systems.VulcanEngine.Unions;
 using ajiva.Utils;
 using ajiva.Utils.Changing;
-using Ajiva.Wrapper.Logger;
 
 namespace ajiva.Systems.VulcanEngine.Systems
 {
     [Dependent(typeof(TextureSystem), typeof(Ajiva3dSystem), typeof(UiRenderer))]
     public class GraphicsSystem : SystemBase, IInit, IUpdate
     {
+        private readonly DeviceSystem deviceSystem;
+        private readonly ImageSystem imageSystem;
+        private readonly WindowSystem windowSystem;
+        private readonly ShaderSystem shaderSystem;
+        private readonly TextureSystem textureSystem;
+        private readonly Ajiva3dSystem ajiva3dSystem;
+        private readonly UiRenderer uiRenderer;
         public IChangingObserver ChangingObserver { get; } = new ChangingObserver(100);
 
         private static readonly object CurrentGraphicsLayoutSwapLock = new();
-        private GraphicsLayout? Current { get; set; }
 
         /// <inheritdoc />
         protected override void ReleaseUnmanagedResources(bool disposing)
         {
-            Current?.Dispose();
-            Current = null;
-        }
-
-        public void EnsureGraphicsLayoutExists()
-        {
-            Current ??= new(Ecs);
-        }
-
-        public void EnsureGraphicsLayoutDeletion()
-        {
-            Current?.DisposeIn(1000);
-            Current = null;
-        }
-
-        public void RecreateCurrentGraphicsLayoutAsync()
-        {
-            new Thread(() =>
-            {
-                var created = new GraphicsLayout(Ecs);
-                created.EnsureExists();
-                GraphicsLayout? old;
-                lock (CurrentGraphicsLayoutSwapLock)
-                {
-                    old = Current;
-                    Current = created;
-                }
-                Thread.Sleep(1000);
-                old?.Dispose();
-            }).Start();
-        }
-
-        public void RecreateCurrentGraphicsLayout()
-        {
-            Ecs.GetSystem<DeviceSystem>().WaitIdle();
-            var created = new GraphicsLayout(Ecs);
-            created.EnsureExists();
-            GraphicsLayout? old;
-            lock (CurrentGraphicsLayoutSwapLock)
-            {
-                old = Current;
-                Current = created;
-            }
-            ChangingObserver.Updated();
-            old?.DisposeIn(1000);
+            renderUnion?.Dispose();
         }
 
         /// <inheritdoc />
         public void Init(AjivaEcs ecs)
         {
-            RecreateCurrentGraphicsLayout();
-            Ecs.GetSystem<WindowSystem>().OnResize += RecreateCurrentGraphicsLayout;
+            RecreateSwapchain();
+            Ecs.GetSystem<WindowSystem>().OnResize += OnResize;
+        }
+
+        private void RecreateSwapchain()
+        {
+            RecreateSwapchainCore();
+        }
+
+        private void OnResize()
+        {
+            RecreateSwapchain();
         }
 
         /// <inheritdoc />
         public void Update(UpdateInfo delta)
         {
-            if (ChangingObserver.UpdateCycle(delta.Iteration)) 
-                RecreateCurrentGraphicsLayout();
-            Current?.DrawFrame();
+            if (ChangingObserver.UpdateCycle(delta.Iteration))
+            {
+                RecreateSwapchainCore();
+                //RefillFrameBuffers();
+            }
+            DrawFrame();
+        }
+
+        private void RefillFrameBuffers()
+        {
+            ChangingObserver.Updated();
+
+            //renderUnion.FillFrameBuffers(ar.ComponentEntityMap.Keys.Union<ARenderAble>(ui.ComponentEntityMap.Keys));
+            renderUnion?.FillFrameBuffers(new Dictionary<AjivaVulkanPipeline, List<ARenderAble>>()
+            {
+                [AjivaVulkanPipeline.Pipeline2d] = ajiva3dSystem.ComponentEntityMap.Keys.Cast<ARenderAble>().ToList(),
+                [AjivaVulkanPipeline.Pipeline3d] = uiRenderer.ComponentEntityMap.Keys.Cast<ARenderAble>().ToList(),
+            });
         }
 
         /// <inheritdoc />
-        public GraphicsSystem(AjivaEcs ecs) : base(ecs)
+        public GraphicsSystem(AjivaEcs ecs, DeviceSystem deviceSystem, ImageSystem imageSystem, WindowSystem windowSystem, ShaderSystem shaderSystem, TextureSystem textureSystem, Ajiva3dSystem ajiva3dSystem, UiRenderer uiRenderer) : base(ecs)
         {
+            this.deviceSystem = deviceSystem;
+            this.imageSystem = imageSystem;
+            this.windowSystem = windowSystem;
+            this.shaderSystem = shaderSystem;
+            this.textureSystem = textureSystem;
+            this.ajiva3dSystem = ajiva3dSystem;
+            this.uiRenderer = uiRenderer;
+        }
+
+        private void RecreateSwapchainCore()
+        {
+            lock (CurrentGraphicsLayoutSwapLock)
+            {
+                renderUnion?.Dispose();
+                depthImage?.Dispose();
+
+                depthImage = deviceSystem.PhysicalDevice.CreateDepthImage(imageSystem, windowSystem.Canvas);
+
+                renderUnion = RenderUnion.CreateRenderUnion(deviceSystem.PhysicalDevice, deviceSystem.Device!, windowSystem.Canvas, shaderSystem, textureSystem.TextureSamplerImageViews, true, depthImage, deviceSystem.CommandPool);
+
+                RefillFrameBuffers();
+            }
+        }
+
+        private RenderUnion? renderUnion;
+        private AImage? depthImage;
+
+        public void DrawFrame()
+        {
+            lock (CurrentGraphicsLayoutSwapLock)
+            {
+                renderUnion.DrawFrame(deviceSystem.GraphicsQueue, deviceSystem.PresentQueue);
+            }
         }
     }
 }
