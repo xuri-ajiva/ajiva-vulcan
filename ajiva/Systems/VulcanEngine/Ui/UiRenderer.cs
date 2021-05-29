b072b889cbbc540be26a0aebae9d3fdb6cdfeab9
xuri02 <t0bl4s.durst@gmail.com>
2021-05-29 21:04:38 +0200

    UiRenderer now implements IAjivaLayer

--- ajiva/Systems/VulcanEngine/Ui/UiRenderer.cs
+++ ajiva/Systems/VulcanEngine/Ui/UiRenderer.cs
@@ -1,21 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ajiva.Components;
+using ajiva.Components.Media;
 using ajiva.Components.RenderAble;
 using ajiva.Ecs;
 using ajiva.Ecs.ComponentSytem;
 using ajiva.Ecs.Entity;
 using ajiva.Ecs.Utils;
+using ajiva.Models;
 using ajiva.Models.Buffer;
+using ajiva.Systems.VulcanEngine.Layer;
 using ajiva.Systems.VulcanEngine.Systems;
 using ajiva.Systems.VulcanEngine.Unions;
 using ajiva.Utils;
 using GlmSharp;
+using SharpVk;
 
 namespace ajiva.Systems.VulcanEngine.Ui
 {
     [Dependent(typeof(ShaderSystem), typeof(WindowSystem))]
-    public class UiRenderer : ComponentSystemBase<ARenderAble2D>, IUpdate, IInit
+    public class UiRenderer : ComponentSystemBase<RenderMesh2D>, IUpdate, IInit, IAjivaLayer
     {
-
         /// <inheritdoc />
         public override void AttachNewComponent(IEntity entity)
         {
@@ -23,14 +29,13 @@ namespace ajiva.Systems.VulcanEngine.Ui
         }
 
         /// <inheritdoc />
-        public override ARenderAble2D CreateComponent(IEntity entity)
+        public override RenderMesh2D CreateComponent(IEntity entity)
         {
             Ecs.GetSystem<GraphicsSystem>().ChangingObserver.Changed();
-            var comp = new ARenderAble2D();
+            var comp = new RenderMesh2D();
             ComponentEntityMap.Add(comp, entity);
             return comp;
         }
-
 
         /// <inheritdoc />
         public void Update(UpdateInfo delta)
@@ -43,6 +48,15 @@ namespace ajiva.Systems.VulcanEngine.Ui
             }
     
             union.UniformModels.CopyRegions(ids);      */
+
+            lock (updatedIds)
+            {
+                if (updatedIds.Count != 0)
+                {
+                    union.UniformModels.CopyRegions(updatedIds);
+                    updatedIds.Clear();
+                }
+            }
         }
 
         /// <inheritdoc />
@@ -72,10 +86,19 @@ namespace ajiva.Systems.VulcanEngine.Ui
                     MouseMoved(args.Pos);
                 }
             };
+
+            PipelineDescriptorInfos = ajiva.Systems.VulcanEngine.Unions.PipelineDescriptorInfos.CreateFrom(
+                Ecs.GetSystem<ShaderSystem>().ShaderUnions[AjivaEngineLayer.Layer2d].ViewProj,
+                Ecs.GetSystem<ShaderSystem>().ShaderUnions[AjivaEngineLayer.Layer2d].UniformModels,
+                Ecs.GetComponentSystem<TextureSystem, ATexture>().TextureSamplerImageViews
+            );
+            Canvas = window.Canvas;
         }
 
         private ShaderUnion? union;
         private WindowSystem window;
+
+        public List<uint> updatedIds = new();
 
         private void MouseMoved(vec2 pos)
         {
@@ -83,19 +106,17 @@ namespace ajiva.Systems.VulcanEngine.Ui
             //LogHelper.Log(posNew);
             if (ComponentEntityMap.Count > 0)
             {
-                var ids = new List<uint>();
-                foreach (var entity in ComponentEntityMap.Keys)
-                {
-                    ids.Add(entity.Id);
-                    union.UniformModels.UpdateExpresionOne(entity.Id, (uint index, ref UniformModel value) =>
+                lock (updatedIds)
+                    foreach (var entity in ComponentEntityMap.Keys)
                     {
-                        value.Model = mat4.Translate(posNew.x, posNew.y, 0) * mat4.Scale(.1f);
-                        return true;
-                    });
-                    union.UniformModels.UpdateOne(new() {Model = mat4.Translate(posNew.x, posNew.y, 0) * mat4.Scale(.1f)}, entity.Id);
-                }
-
-                union.UniformModels.CopyRegions(ids);
+                        updatedIds.Add(entity.Id);
+                        union.UniformModels.UpdateExpresionOne(entity.Id, (uint index, ref UniformModel value) =>
+                        {
+                            value.Model = mat4.Translate(posNew.x, posNew.y, 0) * mat4.Scale(.1f);
+                            return true;
+                        });
+                        union.UniformModels.UpdateOne(new() {Model = mat4.Translate(posNew.x, posNew.y, 0) * mat4.Scale(.1f)}, entity.Id);
+                    }
 
                 /*var cmp = ComponentEntityMap.Keys.First();
 
@@ -114,5 +135,46 @@ namespace ajiva.Systems.VulcanEngine.Ui
         public UiRenderer(AjivaEcs ecs) : base(ecs)
         {
         }
+
+#region Layer
+
+        /// <inheritdoc />
+        public AjivaVulkanPipeline PipelineLayer { get; } = AjivaVulkanPipeline.Pipeline2d;
+
+        /// <inheritdoc />
+        public AImage DepthImage { get; set; }
+
+        /// <inheritdoc />
+        public Shader MainShader
+        {
+            get => Ecs.GetSystem<ShaderSystem>().ShaderUnions[AjivaEngineLayer.Layer2d].Main;
+            set => throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public PipelineDescriptorInfos[] PipelineDescriptorInfos { get; set; }
+
+        /// <inheritdoc />
+        public bool DepthEnabled { get; set; } = false;
+
+        /// <inheritdoc />
+        public Canvas Canvas { get; set; }
+
+        /// <inheritdoc />
+        public VertexInputBindingDescription VertexInputBindingDescription { get; } = Vertex2D.GetBindingDescription();
+
+        /// <inheritdoc />
+        public VertexInputAttributeDescription[] VertexInputAttributeDescriptions { get; } = Vertex2D.GetAttributeDescriptions();
+
+        /// <inheritdoc />
+        public ClearValue[] ClearValues { get; set; } = Array.Empty<ClearValue>();
+
+        /// <inheritdoc />
+        public List<IRenderMesh> GetRenders()
+        {
+            return ComponentEntityMap.Keys.Cast<IRenderMesh>().ToList();
+        }
+
+  #endregion
     }
 }
