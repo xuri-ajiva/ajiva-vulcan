151b2fc842fc10041de86031246d41ada4da2cb1
xuri02 <t0bl4s.durst@gmail.com>
2021-05-29 20:43:31 +0200

    Mesh now implements the IMesh and uses INext for an unique id

--- ajiva/Components/RenderAble/Mesh.cs
+++ ajiva/Components/RenderAble/Mesh.cs
@@ -6,7 +6,7 @@ using SharpVk;
 
 namespace ajiva.Components.RenderAble
 {
-    public partial class Mesh<T> : DisposingLogger where T : struct
+    public class Mesh<T> : DisposingLogger, IMesh where T : struct
     {
         public readonly T[] VerticesData;
         public readonly ushort[] IndicesData;
@@ -18,8 +18,14 @@ namespace ajiva.Components.RenderAble
         {
             this.VerticesData = verticesData;
             this.IndicesData = indicesData;
+            
+            MeshId = INextId<IMesh>.Next();
         }
 
+        /// <inheritdoc />
+        public uint MeshId { get; set; }
+
+        /// <inheritdoc />
         public void Create(DeviceSystem system)
         {
             if (deviceComponent != null) return; // if we have an deviceComponent we are created!
@@ -31,7 +37,7 @@ namespace ajiva.Components.RenderAble
         private BufferOfT<TV> CreateShaderBuffer<TV>(TV[] val, BufferUsageFlags bufferUsage) where TV : struct
         {
             ATrace.Assert(deviceComponent != null, nameof(deviceComponent) + " != null");
-            
+
             BufferOfT<TV> aBuffer = new(val);
             var copyBuffer = CopyBuffer<TV>.CreateCopyBufferOnDevice(val, deviceComponent);
 
@@ -47,8 +53,10 @@ namespace ajiva.Components.RenderAble
         {
             Vertices?.Dispose();
             Indeces?.Dispose();
+            INextId<IMesh>.Remove(MeshId);
         }
 
+        /// <inheritdoc />
         public void Bind(CommandBuffer commandBuffer)
         {
             ATrace.Assert(Vertices != null, nameof(Vertices) + " != null");
@@ -57,6 +65,7 @@ namespace ajiva.Components.RenderAble
             commandBuffer.BindIndexBuffer(Indeces.Buffer, 0, IndexType.Uint16);
         }
 
+        /// <inheritdoc />
         public void DrawIndexed(CommandBuffer commandBuffer)
         {
             ATrace.Assert(Indeces != null, nameof(Indeces) + " != null");
