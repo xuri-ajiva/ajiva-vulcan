14453fe7b70384e826aa3c538ffcf8083d7863f6
xuri02 <t0bl4s.durst@gmail.com>
2021-05-31 15:38:05 +0200

    Added more overloads to PipelineDescriptorInfos

--- ajiva/Systems/VulcanEngine/Unions/PipelineDescriptorInfos.cs
+++ ajiva/Systems/VulcanEngine/Unions/PipelineDescriptorInfos.cs
@@ -17,10 +17,14 @@ namespace ajiva.Systems.VulcanEngine.Unions
 
         public static PipelineDescriptorInfos[] CreateFrom(IBufferOfT viewProj, IBufferOfT uniformModels, DescriptorImageInfo[] textureSamplerImageViews)
         {
+            return CreateFrom(viewProj.Buffer!, viewProj.SizeOfT, uniformModels.Buffer!, uniformModels.SizeOfT, textureSamplerImageViews);
+        }        
+        public static PipelineDescriptorInfos[] CreateFrom(Buffer viewProj, uint sizeOfViewProj, Buffer uniformModels,uint sizeOfModels, DescriptorImageInfo[] textureSamplerImageViews)
+        {
             return new PipelineDescriptorInfos[]
             {
-                new(DescriptorType.UniformBuffer, ShaderStageFlags.Vertex, 0, 1, BufferInfo: new[] {new DescriptorBufferInfo {Buffer = viewProj.Buffer, Offset = 0, Range = viewProj.SizeOfT}}),
-                new(DescriptorType.UniformBufferDynamic, ShaderStageFlags.Vertex, 1, 1, BufferInfo: new[] {new DescriptorBufferInfo {Buffer = uniformModels.Buffer, Offset = 0, Range = uniformModels.SizeOfT}}),
+                new(DescriptorType.UniformBuffer, ShaderStageFlags.Vertex, 0, 1, BufferInfo: new[] {new DescriptorBufferInfo {Buffer = viewProj, Offset = 0, Range = sizeOfViewProj}}),
+                new(DescriptorType.UniformBufferDynamic, ShaderStageFlags.Vertex, 1, 1, BufferInfo: new[] {new DescriptorBufferInfo {Buffer = uniformModels, Offset = 0, Range = sizeOfModels}}),
                 new(DescriptorType.CombinedImageSampler, ShaderStageFlags.Fragment, 2, (uint)textureSamplerImageViews.Length, ImageInfo: textureSamplerImageViews),
             };
         }
