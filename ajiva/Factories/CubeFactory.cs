8d0bd5d3eea1d5fec1e314d4fc342bd8c087ca3f
xuri02 <t0bl4s.durst@gmail.com>
2021-09-23 12:08:17 +0200

    Updated Factory's

--- ajiva/Factories/CubeFactory.cs
+++ ajiva/Factories/CubeFactory.cs
@@ -1,5 +1,6 @@
 ﻿using ajiva.Components.Media;
 using ajiva.Components.RenderAble;
+using ajiva.Components.Transform;
 using ajiva.Ecs;
 using ajiva.Ecs.Factory;
 using ajiva.Entities;
@@ -12,6 +13,7 @@ namespace ajiva.Factories
         public override Cube Create(AjivaEcs system, uint id)
         {
             var cube = new Cube();
+            //return cube.Create3DRenderedObject(system);
             system.AttachComponentToEntity<Transform3d>(cube);
             system.AttachComponentToEntity<RenderMesh3D>(cube);
             //system.AttachComponentToEntity<ATexture>(cube);
@@ -22,7 +24,6 @@ namespace ajiva.Factories
         /// <inheritdoc />
         protected override void ReleaseUnmanagedResources(bool disposing)
         {
-            
         }
     }
 }
--- ajiva/Factories/RectFactory.cs
+++ ajiva/Factories/RectFactory.cs
@@ -1,4 +1,6 @@
-﻿using ajiva.Components.RenderAble;
+﻿using ajiva.Components.Media;
+using ajiva.Components.RenderAble;
+using ajiva.Components.Transform;
 using ajiva.Ecs;
 using ajiva.Ecs.Factory;
 using ajiva.Entities;
@@ -12,6 +14,7 @@ namespace ajiva.Factories
         {
             var rect = new Rect();
             system.AttachComponentToEntity<RenderMesh2D>(rect);
+            system.AttachComponentToEntity<Transform2d>(rect);
             //system.AttachComponentToEntity<ATexture>(cube);
             return rect;
         }
