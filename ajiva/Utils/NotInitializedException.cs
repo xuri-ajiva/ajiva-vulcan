ed1ca11bcccaf2b93604c1241ed401d487d515cf
xuri02 <t0bl4s.durst@gmail.com>
2021-09-23 12:17:55 +0200

    Added NotInitializedException

---
+++ ajiva/Utils/NotInitializedException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ajiva.Utils
+{
+    public class NotInitializedException : Exception
+    {
+        public NotInitializedException(string name, object? obj) : base($"The {obj?.GetType().Name} was not initialized", obj is null ? new NullReferenceException($"{name} was null!") : new ArgumentException($"{obj.GetType()} has an filed that is null"))
+        {
+        }
+    }
+}
