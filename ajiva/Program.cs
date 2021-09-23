f52387c7065b6d1a42e50a066a16f071b8e3482c
xuri02 <t0bl4s.durst@gmail.com>
2021-09-23 12:17:31 +0200

    Added shader compile

--- ajiva/Program.cs
+++ ajiva/Program.cs
@@ -1,9 +1,13 @@
 ﻿//#define TEST_MODE
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ajiva.Application;
+using ajiva.Ecs.Utils;
 using Ajiva.Wrapper.Logger;
 using SharpVk.Glfw;
 using SharpVk.Interop;
@@ -14,13 +18,15 @@ namespace ajiva
     {
         private static void Main()
         {
+            CompileShaders();
+
             LogHelper.UseConsoleCursorPos = false;
             Console.WriteLine($"ProcessId: {Environment.ProcessId}");
             Console.WriteLine($"Version: {Environment.Version}");
             Console.WriteLine($"Is64BitProcess: {Environment.Is64BitProcess}");
             Console.WriteLine($"Is64BitOperatingSystem: {Environment.Is64BitOperatingSystem}");
             Console.WriteLine($"OSVersion: {Environment.OSVersion}");
-            
+
             Glfw3.Init();
 
             var app01 = new AjivaApplication();
@@ -43,6 +49,78 @@ namespace ajiva
             Environment.Exit(0);
         }
 
+        private static void CompileShaders()
+        {
+            /*
+             * ./tools/glslangValidator.exe shader.frag shader.vert -V
+             */
+
+            Task.WaitAll(
+                new DirectoryInfo("Shaders")
+                    .EnumerateDirectories("", SearchOption.AllDirectories)
+                    .Select(shaderDirectory =>
+                        Task.Run(
+                            async () => await CheckAndCompilerShadersInDir(shaderDirectory)
+                        )
+                    ).ToArray()
+            );
+        }
+
+        static string ShaderCompiler = Path.GetFullPath("./tools/spirv/glslangValidator.exe");
+
+        private static async Task CheckAndCompilerShadersInDir(DirectoryInfo shaderDirectory)
+        {
+            var files = shaderDirectory.GetFiles();
+            var vert = files.FirstOrDefault(x => x.Extension == ".vert");
+            var frag = files.FirstOrDefault(x => x.Extension == ".frag");
+
+            if (vert is null && frag is null)
+                return;
+            if (vert is null)
+            {
+                Console.WriteLine($"[COMPILE/ERROR] Vertex Shader is Null! for: {shaderDirectory.Name}");
+                return;
+            }
+            if (frag is null)
+            {
+                Console.WriteLine($"[COMPILE/ERROR] Fragment Shader is Null! for: {shaderDirectory.Name}");
+                return;
+            }
+
+            var compiler = new Process
+            {
+                StartInfo = new ProcessStartInfo(ShaderCompiler, $"{frag.Name} {vert.Name} -V")
+                {
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    WorkingDirectory = shaderDirectory.FullName,
+                    CreateNoWindow = true
+                },
+            };
+            compiler.Start();
+            compiler.PriorityClass = ProcessPriorityClass.High;
+            compiler.EnableRaisingEvents = true;
+            compiler.PriorityBoostEnabled = true;
+            while (!compiler.HasExited)
+            {
+                compiler.Refresh();
+                await Task.Delay(100);
+            }
+            var errors = await compiler.StandardError.ReadToEndAsync();
+            var output = await compiler.StandardOutput.ReadToEndAsync();
+
+            lock (ConsoleLock)
+            {
+                Console.WriteLine($"[COMPILE/INFO]: Shaders for: {shaderDirectory.Name}");
+                if (!string.IsNullOrEmpty(output))
+                    Console.WriteLine($"[COMPILE/RESULT/INFO] {output}");
+                if (!string.IsNullOrEmpty(errors))
+                    Console.WriteLine($"[COMPILE/RESULT/ERROR] {errors}");
+                Console.WriteLine($"[COMPILE/RESULT/EXIT] Compiler Process has exited with code {compiler.ExitCode}");
+            }
+        }
+
+        private static object ConsoleLock = new();
         private static readonly CancellationTokenSource TaskSource = new();
         private static readonly List<Task> Tasks = new();
 
