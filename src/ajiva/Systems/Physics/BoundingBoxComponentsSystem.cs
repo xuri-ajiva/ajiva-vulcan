8d66b45d91d5b6156208f4a5d2916220578b2891
xuri02 <t0bl4s.durst@gmail.com>
2021-10-29 19:20:16 +0200

    Added Collision resolver

--- src/ajiva/Systems/Physics/BoundingBoxComponentsSystem.cs
+++ src/ajiva/Systems/Physics/BoundingBoxComponentsSystem.cs
@@ -1,10 +1,14 @@
-﻿using ajiva.Components.Physics;
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using ajiva.Components.Physics;
 using ajiva.Components.Transform;
 using ajiva.Ecs;
 using ajiva.Ecs.ComponentSytem;
 using ajiva.Ecs.Entity;
 using ajiva.Ecs.Utils;
 using ajiva.Utils;
+using GlmSharp;
 
 namespace ajiva.Systems.Physics
 {
@@ -18,6 +22,158 @@ namespace ajiva.Systems.Physics
         public void Update(UpdateInfo delta)
         {
         }
+
+        public void DoPhysicFrame()
+        {
+            foreach (var (box1, _) in ComponentEntityMap.Where(x => !x.Key.Collider.IsStatic))
+            {
+                foreach (var (box2, _) in ComponentEntityMap.Where(other => box1 != other.Key))
+                {
+                    DoCollision(box1, box2);
+                }
+            }
+        }
+
+        public void DoCollision(BoundingBox b1, BoundingBox b2)
+        {
+            if (!Intersect(b1, b2)) return;
+
+            var resolved = ResolveColision(b1, b2);
+            if (b2.Collider.IsStatic)
+            {
+                b1.ModifyPositionRelative(resolved);
+            }
+            else
+            {
+                b1.ModifyPositionRelative(resolved / 2);
+                b2.ModifyPositionRelative(-resolved / 2);
+            }
+        }
+
+        /*
+         * sorce:
+         *
+function resolveCollision(A, B) {
+    // get the vectors to check against
+    var vX = (A.x + (A.w / 2))  - (B.x + (B.w / 2)),
+        vY = (A.y + (A.h / 2)) - (B.y + (B.h / 2)),
+        // Half widths and half heights of the objects
+        ww2 = (A.w / 2) + (B.w / 2),
+        hh2 = (A.h / 2) + (B.h / 2),
+        colDir = "";
+
+    // if the x and y vector are less than the half width or half height,
+    // they we must be inside the object, causing a collision
+    if (Math.abs(vX) < ww2 && Math.abs(vY) < hh2) {
+        // figures out on which side we are colliding (top, bottom, left, or right)
+        var oX = ww2 - Math.abs(vX),
+            oY = hh2 - Math.abs(vY);
+        if (oX >= oY) {
+            if (vY > 0) {
+                colDir = "TOP";
+                A.y += oY;
+            } else {
+                colDir = "BOTTOM";
+                A.y -= oY;
+            }
+        } else {
+            if (vX > 0) {
+                colDir = "LEFT";
+                A.x += oX;
+            } else {
+                colDir = "RIGHT";
+                A.x -= oX;
+            }
+        }
+    }
+    return colDir; // If you need info of the side that collided
+}
+         */
+        private vec3 ResolveColision(BoundingBox a, BoundingBox b)
+        {
+            var v = a.Center - b.Center;
+            var hh = a.SizeHalf + b.SizeHalf;
+
+            const float mm = 0.0001f;
+            float rx = 0,
+                ry = 0,
+                rz = 0;
+
+            if (!(Math.Abs(v.x) < hh.x) || !(Math.Abs(v.y) < hh.y) || !(Math.Abs(v.z) < hh.z)) return vec3.Zero;
+            var o = hh - vec3.Abs(v);
+            if (o.x >= o.y)
+            {
+                if (v.y > 0)
+                {
+                    ry += o.y;
+                }
+                else
+                {
+                    ry -= o.y;
+                }
+            }
+            else if (o.x >= o.z)
+            {
+                if (v.z > 0)
+                {
+                    rz += o.z;
+                }
+                else
+                {
+                    rz -= o.z;
+                }
+            }
+            //if (o.y >= o.z)
+            else
+            {
+                if (v.x > 0)
+                {
+                    rx += o.x;
+                }
+                else
+                {
+                    rx -= o.x;
+                }
+            }
+            return new vec3(rx, ry, rz);
+        }
+
+#region IntersectionTest
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool Intersect(BoundingBox a, BoundingBox b)
+        {
+            return IntersectX(a, b) &&
+                   IntersectY(a, b) &&
+                   IntersectZ(a, b);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool IntersectZ(BoundingBox a, BoundingBox b)
+        {
+            return (a.MinPos.z < b.MaxPos.z && a.MaxPos.z > b.MinPos.z);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool IntersectY(BoundingBox a, BoundingBox b)
+        {
+            return (a.MinPos.y < b.MaxPos.y && a.MaxPos.y > b.MinPos.y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool IntersectX(BoundingBox a, BoundingBox b)
+        {
+            return (a.MinPos.x < b.MaxPos.x && a.MaxPos.x > b.MinPos.x);
+        }
+
+        private static bool IsPointInsideAabb(vec3 point, BoundingBox box)
+        {
+            return (point.x > box.MinPos.x && point.x < box.MaxPos.x) &&
+                   (point.y > box.MinPos.y && point.y < box.MaxPos.y) &&
+                   (point.z > box.MinPos.z && point.z < box.MaxPos.z);
+        }
+
+#endregion
 
         /// <inheritdoc />
         public override BoundingBox RegisterComponent(IEntity entity, BoundingBox component)
