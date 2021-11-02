2b13a3e2da6499042e2c350116ed17931bf537fc
xuri02 <t0bl4s.durst@gmail.com>
2021-11-02 13:19:21 +0100

    Added Reactive<T>

---
+++ src/ajiva/Reactive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ajiva
+{
+    public class Reactive<T> where T : IEquatable<T>, new()
+    {
+        public delegate void OnChangeDelegate(ref T oldState, ref T newState);
+
+        public event OnChangeDelegate? OnChange;
+        private T value;
+        public T Value
+        {
+            get => value;
+            set
+            {
+                if(this.value.Equals(value)) return;
+                var copy = this.value;
+                this.value = value;
+                OnChange?.Invoke(ref copy, ref value);
+            }
+        }
+
+        public Reactive(T value)
+        {
+            this.value = value;
+        }
+        
+        public static implicit operator T(Reactive<T> reactive) => reactive.Value;
+        public static implicit operator Reactive<T>(T value) => new Reactive<T>(value);
+    }
+}
