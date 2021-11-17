faa9b2ad63e6bbb862ddf2448609c8da4cdf3237
xuri02 <t0bl4s.durst@gmail.com>
2021-11-17 15:33:21 +0100

    Updated PeriodicUpdateRunner to not consume 100% cpu

--- src/ajiva.Ecs/Utils/PeriodicUpdateRunner.cs
+++ src/ajiva.Ecs/Utils/PeriodicUpdateRunner.cs
@@ -3,67 +3,106 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using Ajiva.Wrapper.Logger;
 
 namespace ajiva.Ecs.Utils;
 
+public record PeriodicUpdateInfo(TimeSpan Interval);
+
 public class PeriodicUpdateRunner
 {
-    private record UpdateData(Thread Runner, CancellationTokenSource Source);
+    private class UpdateData
+    {
+        public long Iteration;
+        public Thread Runner;
+        public readonly CancellationTokenSource Source;
+        public long Delta;
 
-    private Dictionary<IUpdate, UpdateData> UpdateDatas = new();
+        public UpdateData()
+        {
+            this.Source = new CancellationTokenSource();
+            Iteration = 0L;
+        }
+    }
+
+    private readonly Dictionary<IUpdate, UpdateData> updateDatas = new();
 
     public void RegisterUpdate(IUpdate update)
     {
-        var source = new CancellationTokenSource();
+        var data = new UpdateData();
         var runner = new Thread(() =>
         {
-            RunDelta(update, source.Token);
+            RunDelta(update, data);
         }) { Name = $"Update Runner for {update}" };
-        UpdateDatas.Add(update, new UpdateData(runner, source));
+        data.Runner = runner;
+        updateDatas.Add(update, data);
     }
 
-    private void RunDelta(IUpdate update, CancellationToken cancellationToken)
+    private void RunDelta(IUpdate update, UpdateData data)
     {
-        var iteration = 0L;
-        var delta = update.Info.Interval.Ticks;
+        data.Iteration = 0L;
+        data.Delta = update.Info.Interval.Ticks;
         var now = Stopwatch.GetTimestamp();
 
         var ticks = update.Info.Interval.Ticks;
-        while (!cancellationToken.IsCancellationRequested)
+        while (!data.Source.IsCancellationRequested)
         {
-            update.Update(new UpdateInfo(new TimeSpan(delta), iteration));
+            update.Update(new UpdateInfo(new TimeSpan(data.Delta), data.Iteration));
 
-            iteration++;
+            data.Iteration++;
+
+            var elapsed = Stopwatch.GetTimestamp() - now;
+            var remaining = (ticks - elapsed);
+            if (remaining / 10000 > 0)
+            {
+                Thread.Sleep((int)(remaining / 10000) - 1);
+            }
             while (Stopwatch.GetTimestamp() - now < ticks)
             {
                 Thread.Yield();
             }
 
             var end = Stopwatch.GetTimestamp();
-            delta = end - now;
+            data.Delta = end - now;
             now = end;
         }
     }
 
     public void Start(IUpdate update)
     {
-        UpdateDatas[update].Runner.Start();
+        updateDatas[update].Runner.Start();
     }
 
     public void Cancel(IUpdate update)
     {
-        UpdateDatas[update].Source.Cancel();
+        updateDatas[update].Source.Cancel();
     }
 
     public async Task WaitHandle(CancellationToken cancellation)
     {
-        foreach (var (key, value) in UpdateDatas)
+        DateTime begin = DateTime.Now;
+        foreach (var (key, value) in updateDatas)
         {
             while (value.Runner.IsAlive)
             {
+                if ((DateTime.Now - begin).Seconds > 20)
+                {
+                    begin = DateTime.Now;
+                    LogStatus();
+                }
+
                 await Task.Delay(10, cancellation);
                 if (cancellation.IsCancellationRequested) return;
             }
+        }
+    }
+
+    private void LogStatus()
+    {
+        ALog.Info(new string('-', 100));
+        foreach (var (key, value) in updateDatas)
+        {
+            ALog.Info($"[ITERATION:{value.Iteration:X8}] | {value.Iteration.ToString(),-8}| {key.GetType().Name,-40}: Delta: {new TimeSpan(value.Delta):G}");
         }
     }
 }
