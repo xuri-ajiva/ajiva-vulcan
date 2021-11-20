0c6f1b6a40b0b4e5348d01d3ed952d5df5738b33
xuri02 <t0bl4s.durst@gmail.com>
2021-11-20 19:45:47 +0100

    IEntityFactory now handles Delete

--- src/ajiva.Ecs/Factory/IEntityFactory.cs
+++ src/ajiva.Ecs/Factory/IEntityFactory.cs
@@ -5,6 +5,15 @@ namespace ajiva.Ecs.Factory;
 public interface IEntityFactory : IDisposable, IAjivaEcsObject
 {
     IEntity Create(IAjivaEcs system, uint id);
+
+    void Delete(IAjivaEcs ecs, IEntity entity)
+    {
+        foreach (var (_, value) in entity.Components)
+        {
+            ecs.UnRegisterComponent(entity, value);
+        }
+        entity.Dispose();
+    }
 }
 public interface IEntityFactory<T> : IEntityFactory where T : class, IEntity
 {
