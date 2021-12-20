ecc0a6cb4050303586e19d5427200ebb18840114
xuri02 <t0bl4s.durst@gmail.com>
2021-12-20 14:34:32 +0100

    Added Dynamic Dedicated Buffers for Arrays

--- src/ajiva/Models/Buffer/ABuffer.cs
+++ src/ajiva/Models/Buffer/ABuffer.cs
@@ -3,7 +3,7 @@ using SharpVk;
 
 namespace ajiva.Models.Buffer;
 
-public class ABuffer : DisposingLogger
+public class ABuffer : DisposingLogger, IEquatable<ABuffer>
 {
     static object _lock = new object();
 
@@ -18,8 +18,6 @@ public class ABuffer : DisposingLogger
 
     public void Create(IDeviceSystem system, BufferUsageFlags usage, MemoryPropertyFlags flags)
     {
-        //todo: system.EnsureDevicesExist();
-
         Buffer = system.Device!.CreateBuffer(Size, usage, SharingMode.Exclusive, null);
 
         var memRequirements = Buffer.GetMemoryRequirements();
@@ -27,11 +25,18 @@ public class ABuffer : DisposingLogger
         Memory = system.Device.AllocateMemory(memRequirements.Size, system.FindMemoryType(memRequirements.MemoryTypeBits, flags));
         Buffer.BindMemory(Memory, 0);
         system.WatchObject(this);
+    }    
+    public static ABuffer Create(IDeviceSystem system, uint size, BufferUsageFlags usage, MemoryPropertyFlags flags)
+    {
+        var res = new ABuffer(size);
+        res.Create(system, usage, flags);
+        return res;
     }
 
     /// <inheritdoc />
     protected override void ReleaseUnmanagedResources(bool disposing)
     {
+        ALog.Info($"Buffer Deleted: {Buffer.RawHandle}, Disposing: {disposing}");
         Buffer?.Dispose();
         Memory?.Free();
     }
@@ -86,5 +91,28 @@ public class ABuffer : DisposingLogger
         {
             return Ptr.ToPointer();
         }
+    }
+
+    /// <inheritdoc />
+    public bool Equals(ABuffer? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Equals(Buffer, other.Buffer) && Equals(Memory, other.Memory) && Size == other.Size;
+    }
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((ABuffer)obj);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Buffer, Memory, Size);
     }
 }
---
+++ src/ajiva/Models/Buffer/Dynamic/DynamicDedicatedBuffer.cs
@@ -0,0 +1,97 @@
+﻿using ajiva.Systems.VulcanEngine.Interfaces;
+using ajiva.Systems.VulcanEngine.Systems;
+using ajiva.Utils.Changing;
+using SharpVk;
+
+namespace ajiva.Models.Buffer.Dynamic;
+
+public class ResizableDedicatedBuffer : DisposingLogger, IEquatable<ResizableDedicatedBuffer>
+{
+    private readonly IDeviceSystem system;
+    private readonly BufferUsageFlags usage;
+    private readonly MemoryPropertyFlags flags;
+
+    public IChangingObserver<ResizableDedicatedBuffer> BufferResized { get; }
+
+    public ResizableDedicatedBuffer(uint size, IDeviceSystem system, BufferUsageFlags usage, MemoryPropertyFlags flags)
+    {
+        BufferResized = new ChangingObserver<ResizableDedicatedBuffer>(this);
+        Buffer = new Reactive<ABuffer>(null);
+        Size = size;
+        this.system = system;
+        this.usage = usage;
+        this.flags = flags;
+        Resize(Size);
+        system.WatchObject(this);
+    }
+
+    public Reactive<ABuffer> Buffer { get; }
+    public uint Size { get; protected set; }
+
+    /// <inheritdoc />
+    protected override void ReleaseUnmanagedResources(bool disposing)
+    {
+        Buffer.Value?.Dispose();
+    }
+
+    public void Resize(uint newSize)
+    {
+        lock (this)
+        {
+            Size = newSize;
+            //system.ToBeDeleted(Buffer.Value);  // let gc decide when to delete the buffer
+            if (newSize == 0)
+            {
+                BufferResized.Changed();
+                return;
+            }
+
+            Buffer.Value = ABuffer.Create(system, newSize, usage, flags);
+            BufferResized.Changed();
+        }
+    }
+
+    public ABuffer.DisposablePointer MapDisposer()
+    {
+        lock (this)
+        {
+            return Buffer.Value!.MapDisposer();
+        }
+    }
+
+
+    public void CopyToRegions(ResizableDedicatedBuffer destination, ArrayProxy<BufferCopy> regions)
+    {
+        if (destination.Size < Size) throw new ArgumentException("The Destination Buffer is smaller than the Source Buffer", nameof(destination));
+
+        system.QueueSingleTimeCommand(QueueType.TransferQueue, CommandPoolSelector.Transit, command =>
+        {
+            command.CopyBuffer(Current().Buffer, destination.Current().Buffer, regions);
+        });
+    }
+
+    public ABuffer Current() => Buffer.Value!;
+
+    /// <inheritdoc />
+    public bool Equals(ResizableDedicatedBuffer? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return usage == other.usage && Size == other.Size && flags == other.flags && Buffer.Equals(other.Buffer) ;
+    }
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        return !ReferenceEquals(null, obj)
+               && (ReferenceEquals(this, obj)
+                   || obj.GetType() == GetType()
+                   && Equals((ResizableDedicatedBuffer)obj));
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return HashCode.Combine((int)usage, (int)flags, Buffer, Size);
+    }
+}
