a36c98e1b1dd5a55a942cc6aafa1a953b30d97e5
Tobias Durst <t0bl4s.durst@gmail.com>
2023-04-20 16:50:52 +0200

    Added IEntityRegistry

---
+++ src/ajiva.Ecs/IEntityRegistry.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+using System.Diagnostics.CodeAnalysis;
+
+namespace ajiva.Ecs;
+
+public interface IEntityRegistry
+{
+    long EntitiesCount { get; }
+    void RegisterEntity<T>(T entity) where T : class, IEntity;
+    bool TryUnRegisterEntity<T>(uint id, [MaybeNullWhen(false)] out T entity) where T : IEntity;
+    bool TryUnRegisterEntity<T>(T entity) where T : IEntity;
+}       
