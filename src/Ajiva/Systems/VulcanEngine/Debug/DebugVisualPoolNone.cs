0d2f8c89423a32d87baa8dfaa38bba354981a840
Tobias Durst <t0bl4s.durst@gmail.com>
2023-04-22 14:15:05 +0200

    Added IDebugVisualPool

---
+++ src/Ajiva/Systems/VulcanEngine/Debug/DebugVisualPoolNone.cs
@@ -0,0 +1,18 @@
+﻿using Ajiva.Components.Transform.SpatialAcceleration;
+
+namespace Ajiva.Systems.VulcanEngine.Debug;
+
+public class DebugVisualPoolNone : IDebugVisualPool
+{
+    public void UpdateVisual(object owner, StaticOctalSpace area)
+    {
+    }
+
+    public void DestroyVisual(object owner)
+    {
+    }
+
+    public void CreateVisual(object owner, StaticOctalSpace area)
+    {
+    }
+}
---
+++ src/Ajiva/Systems/VulcanEngine/Debug/IDebugVisualPool.cs
@@ -0,0 +1,10 @@
+﻿using Ajiva.Components.Transform.SpatialAcceleration;
+
+namespace Ajiva.Systems.VulcanEngine.Debug;
+
+public interface IDebugVisualPool
+{
+    void UpdateVisual(object owner, StaticOctalSpace area);
+    void DestroyVisual(object owner);
+    void CreateVisual(object owner, StaticOctalSpace area);
+}
